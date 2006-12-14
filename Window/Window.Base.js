048061aa376bc60a31c2b582d7dde2e4f24ae01b
chris <chris@4db308c1-fb21-0410-9919-de62e267375e>
2006-12-14 18:31:39 +0000

    Smaller code for window.addEvent (onDomReady)
    git-svn-id: http://svn.mootools.net/trunk@141 4db308c1-fb21-0410-9919-de62e267375e

--- Window/Window.Base.js
+++ Window/Window.Base.js
@@ -16,7 +16,7 @@ window.extend = Object.extend;
 window.extend({
 
 	/*
-	Function: Window.disableImageCache
+	Function: window.disableImageCache
 		Disables background image chache for internex explorer, to prevent flickering. 
 		To be called if you have effects with background images, and they flicker.
 
@@ -28,37 +28,18 @@ window.extend({
 		if (this.ie6) try {document.execCommand("BackgroundImageCache", false, true);} catch (e){};
 	},
 
-	/*
-	Function: Window.onDomReady
-		Executes the passed in function when the DOM is ready (when the document tree has loaded, not waiting for images).
-		Same as window.addEvent('domready', init);
-
-	Credits:
-		(c) Dean Edwards/Matthias Miller/John Resig, remastered for mootools. Later touched up by Christophe Beyls <http://digitalia.be>.
-
-	Arguments:
-		init - the function to execute when the DOM is ready
-
-	Example:
-		> window.addEvent('domready', function(){alert('the dom is ready')});
-	*/
-
 	addEvent: function(type, fn){
-		Element.prototype.addEvent.call(this, type, fn);
 		if (type == 'domready'){
 			if (this.loaded) fn();
-			else if (!this.events.ready){
-				this.events.ready = Class.empty;
+			else if (!this.events || !this.events.domready){
 				var domReady = function(){
-					if (arguments.callee.done) return;
-					arguments.callee.done = true;
-					window.loaded = true;
-					if (window.timer) window.timer = $clear(window.timer);
-					for (var init in window.events.domready) window.events.domready[init]();
-					window.events.domready = null;
-					window.events.ready = null;
-				};
-				if (document.readyState && this.khtml){
+					if (this.loaded) return;
+					this.loaded = true;
+					if (this.timer) this.timer = $clear(this.timer);
+					Element.prototype.fireEvent.call(this, 'domready');
+					this.events.domready = null;
+				}.bind(this);
+				if (document.readyState && this.khtml){ //safari and konqueror
 					this.timer = function(){
 						if (['loaded','complete'].test(document.readyState)) domReady();
 					}.periodical(50);
@@ -74,8 +55,24 @@ window.extend({
 				}
 			}
 		}
+		Element.prototype.addEvent.call(this, type, fn);
 		return this;
 	},
+
+	/*
+	Function: window.onDomReady
+		Executes the passed in function when the DOM is ready (when the document tree has loaded, not waiting for images).
+		Same as window.addEvent('domready', init);
+
+	Credits:
+		(c) Dean Edwards/Matthias Miller/John Resig, remastered for mootools. Later touched up by Christophe Beyls <http://digitalia.be>.
+
+	Arguments:
+		init - the function to execute when the DOM is ready
+
+	Example:
+		> window.addEvent('domready', function(){alert('the dom is ready')});
+	*/
 
 	onDomReady: function(init){
 		return this.addEvent('domready', init);
