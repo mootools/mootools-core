e3d07b5e8cbf3446a4d0248e3803d1b0b9c2a64c
meej <meej@4db308c1-fb21-0410-9919-de62e267375e>
2007-01-13 04:52:25 +0000

    Added Array.select, Array.without, and modified Array.remove to accept an array of values to remove. Also added documentation to Array.js
    git-svn-id: http://svn.mootools.net/trunk@198 4db308c1-fb21-0410-9919-de62e267375e

--- Native/Array.js
+++ Native/Array.js
@@ -14,75 +14,75 @@ Class: Array
 	A collection of The Array Object prototype methods.
 */
 
-//emulated methods
-
-/*
-Property: forEach
-	Iterates through an array; This method is only available for browsers without native *forEach* support.
-	For more info see <http://developer.mozilla.org/en/docs/Core_JavaScript_1.5_Reference:Global_Objects:Array:forEach>
-*/
-
-Array.prototype.forEach = Array.prototype.forEach || function(fn, bind){
-	for (var i = 0; i < this.length; i++) fn.call(bind, this[i], i, this);
-};
-
-/*
-Property: map
-	This method is provided only for browsers without native *map* support.
-	For more info see <http://developer.mozilla.org/en/docs/Core_JavaScript_1.5_Reference:Global_Objects:Array:map>
-*/
-
-Array.prototype.map = Array.prototype.map || function(fn, bind){
-	var results = [];
-	for (var i = 0; i < this.length; i++) results[i] = fn.call(bind, this[i], i, this);
-	return results;
-};
-
-/*
-Property: every
-	This method is provided only for browsers without native *every* support.
-	For more info see <http://developer.mozilla.org/en/docs/Core_JavaScript_1.5_Reference:Global_Objects:Array:every>
-*/
-
-Array.prototype.every = Array.prototype.every || function(fn, bind){
-	for (var i = 0; i < this.length; i++){
-		if (!fn.call(bind, this[i], i, this)) return false;
-	}
-	return true;
-};
-
-/*
-Property: some
-	This method is provided only for browsers without native *some* support.
-	For more info see <http://developer.mozilla.org/en/docs/Core_JavaScript_1.5_Reference:Global_Objects:Array:some>
-*/
-
-Array.prototype.some = Array.prototype.some || function(fn, bind){
-	for (var i = 0; i < this.length; i++){
-		if (fn.call(bind, this[i], i, this)) return true;
-	}
-	return false;
-};
-
-/*
-Property: indexOf
-	This method is provided only for browsers without native *indexOf* support.
-	For more info see <http://developer.mozilla.org/en/docs/Core_JavaScript_1.5_Reference:Global_Objects:Array:indexOf>
-*/
-
-Array.prototype.indexOf = Array.prototype.indexOf || function(item, from){
-	from = from || 0;
-	if (from < 0) from = Math.max(0, this.length + from);
-	while (from < this.length){
-		if(this[from] === item) return from;
-		from++;
-	}
-	return -1;
-};
-
-//custom methods
-
 Array.extend({
+
+	//emulated methods
+
+	/*
+	Property: forEach
+		Iterates through an array; This method is only available for browsers without native *forEach* support.
+		For more info see <http://developer.mozilla.org/en/docs/Core_JavaScript_1.5_Reference:Global_Objects:Array:forEach>
+	*/
+	
+	forEach: Array.prototype.forEach || function(fn, bind){
+		for (var i = 0; i < this.length; i++) fn.call(bind, this[i], i, this);
+	},
+	
+	/*
+	Property: map
+		This method is provided only for browsers without native *map* support.
+		For more info see <http://developer.mozilla.org/en/docs/Core_JavaScript_1.5_Reference:Global_Objects:Array:map>
+	*/
+	
+	map: Array.prototype.map || function(fn, bind){
+		var results = [];
+		for (var i = 0; i < this.length; i++) results[i] = fn.call(bind, this[i], i, this);
+		return results;
+	},
+	
+	/*
+	Property: every
+		This method is provided only for browsers without native *every* support.
+		For more info see <http://developer.mozilla.org/en/docs/Core_JavaScript_1.5_Reference:Global_Objects:Array:every>
+	*/
+	
+	every: Array.prototype.every || function(fn, bind){
+		for (var i = 0; i < this.length; i++){
+			if (!fn.call(bind, this[i], i, this)) return false;
+		}
+		return true;
+	},
+	
+	/*
+	Property: some
+		This method is provided only for browsers without native *some* support.
+		For more info see <http://developer.mozilla.org/en/docs/Core_JavaScript_1.5_Reference:Global_Objects:Array:some>
+	*/
+	
+	some: Array.prototype.some || function(fn, bind){
+		for (var i = 0; i < this.length; i++){
+			if (fn.call(bind, this[i], i, this)) return true;
+		}
+		return false;
+	},
+	
+	/*
+	Property: indexOf
+		This method is provided only for browsers without native *indexOf* support.
+		For more info see <http://developer.mozilla.org/en/docs/Core_JavaScript_1.5_Reference:Global_Objects:Array:indexOf>
+	*/
+	
+	indexOf: Array.prototype.indexOf || function(item, from){
+		from = from || 0;
+		if (from < 0) from = Math.max(0, this.length + from);
+		while (from < this.length){
+			if(this[from] === item) return from;
+			from++;
+		}
+		return -1;
+	},
+	
+	//custom methods
 
 	/*
 	Property: each
@@ -100,10 +100,35 @@ Array.extend({
 	*/
 
 	each: Array.prototype.forEach,
+	
+	/*
+	Property: select
+		Returns a new array containing all items of the original array on which the iterator function returns true.
+
+	Arguments:
+		iterator - a function that is used to determine which items will be selected
+	
+	Returns:
+		an Array
+
+	Example:
+		>var letters = ["a","b","c","d","c"];
+		>var countCs = letters.select(function(item, index, array){
+		>	return item == 'c';
+		>}); // ["c","c"]
+	*/
+	
+	select: function(iterator){
+		var array = [];
+		this.forEach(function(item, index){
+			if (iterator(item, index, array)) array.push(item);
+		});
+		return array;
+	},
 
 	/*
 	Property: copy
-		Copy the array and returns it.
+		Returns a copy of the array.
 
 	Returns:
 		an Array
@@ -114,32 +139,56 @@ Array.extend({
 	*/
 
 	copy: function(){
-		var newArray = [];
-		for (var i = 0; i < this.length; i++) newArray[i] = this[i];
-		return newArray;
+		return this.select(function(){
+			return true;
+		});
 	},
 
 	/*
 	Property: remove
-		Removes all occurrences of an item from the array.
+		Removes all occurrences of an item (or an array of items) from the array.
 
 	Arguments:
-		item - the item to remove
+		items - the item (or array of items) to remove
 
 	Returns:
-		the Array with all occurrences of the item removed.
+		the Array with all occurrences of the item(s) removed
 
 	Example:
-		>["1","2","3","2"].remove("2") // ["1","3"];
+		>["1","2","3","2"].remove("2") // ["1","3"]
+		>["1","2","3","2"].remove(["1","2"]) // ["3"]
 	*/
 
-	remove: function(item){
+	remove: function(items){
+		items = items.push ? items : [items];
 		var i = 0;
 		while (i < this.length){
-			if (this[i] == item) this.splice(i, 1);
+			if (items.test(this[i])) this.splice(i, 1);
 			else i++;
 		}
 		return this;
+	},
+	
+	/*
+	Property: without
+		Returns a copy of the array with the specified items ommitted.
+
+	Arguments:
+		items - the item (or array of items) to ommit
+	
+	Returns:
+		an Array
+
+	Example:
+		>var letters = ["a","b","c","d","c","b"];
+		>var AandBs = letters.without(["c","d"]); // ["a","b","b"]
+	*/
+	
+	without: function(items){
+		items = items.push ? items : [items];
+		return this.select(function(item){
+			return !items.test(item);
+		});
 	},
 
 	/*
@@ -165,15 +214,15 @@ Array.extend({
 
 	/*
 	Property: extend
-		Extends an array with another
+		Extends an array with another.
 
 	Arguments:
-		newArray - the array to extend ours with
+		newArray - the array of items to add to the original array
 
 	Example:
-		>var Animals = ['Cat', 'Dog', 'Coala'];
+		>var Animals = ['Cat', 'Dog', 'Koala'];
 		>Animals.extend(['Lizard']);
-		>//Animals is now: ['Cat', 'Dog', 'Coala', 'Lizard'];
+		>//Animals is now: ['Cat', 'Dog', 'Koala', 'Lizard'];
 	*/
 
 	extend: function(newArray){
@@ -183,21 +232,17 @@ Array.extend({
 
 	/*
 	Property: associate
-		Creates an object with key-value pairs based on the array of keywords passed in
-		and the current content of the array.
+		Creates an object with key-value pairs based on the array of keywords passed in and the current content of the array.
 
 	Arguments:
-		keys - the array of keywords.
+		keys - the array of keywords
 
 	Example:
-		(start code)
-		var Animals = ['Cat', 'Dog', 'Coala', 'Lizard'];
-		var Speech = ['Miao', 'Bau', 'Fruuu', 'Mute'];
-		var Speeches = Animals.associate(speech);
-		//Speeches['Miao'] is now Cat.
-		//Speeches['Bau'] is now Dog.
-		//...
-		(end)
+		>var Animals = ['Cat', 'Dog', 'Koala', 'Lizard'];
+		>var Speech = ['Meow', 'Bau', 'Fruuu', 'Mute'];
+		>var Speeches = Animals.associate(speech);
+		>//Speeches['Meow'] is now Cat.
+		>//Speeches['Bau'] is now Dog.
 	*/
 
 	associate: function(keys){
@@ -216,14 +261,12 @@ Function: $A()
 	Useful to apply Array prototypes to iterable objects, as a collection of DOM elements or the arguments object.
 
 Example:
-	(start code)
-	function myFunction(){
-		$A(arguments).each(argument, function(){
-			alert(argument);
-		});
-	};
-	//the above will alert all the arguments passed to the function myFunction.
-	(end)
+	>function myFunction(){
+	>	$A(arguments).each(argument, function(){
+	>		alert(argument);
+	>	});
+	>};
+	>//the above will alert all the arguments passed to the function myFunction.
 */
 
 function $A(array){
