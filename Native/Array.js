24cf2fe52001fbfa39078e767a5be161830b37f4
digitarald <digitarald@4db308c1-fb21-0410-9919-de62e267375e>
2007-03-23 16:02:59 +0000

    - fixed a warning for undefined property in $each
    git-svn-id: http://svn.mootools.net/trunk@402 4db308c1-fb21-0410-9919-de62e267375e

--- Native/Array.js
+++ Native/Array.js
@@ -395,6 +395,6 @@ Examples:
 */
 
 function $each(iterable, fn, bind){
-	if ($chk(iterable.length)) Array.prototype.forEach.call(iterable, fn, bind);
+	if ($chk(iterable.length || null)) Array.prototype.forEach.call(iterable, fn, bind);
 	else for (var name in iterable) fn.call(bind || iterable, iterable[name], name);
 };
