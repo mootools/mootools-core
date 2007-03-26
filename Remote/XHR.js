dad94d0e3b63100429b5a09dc22728b442b31776
digitarald <digitarald@4db308c1-fb21-0410-9919-de62e267375e>
2007-03-26 16:11:32 +0000

    - XHR does now also append the data querystring to url's that already have a querystring
    git-svn-id: http://svn.mootools.net/trunk@416 4db308c1-fb21-0410-9919-de62e267375e

--- Remote/XHR.js
+++ Remote/XHR.js
@@ -117,7 +117,7 @@ var XHR = new Class({
 		if (this.options.autoCancel) this.cancel();
 		else if (this.running) return this;
 		this.running = true;
-		if (data && this.options.method == 'get' && !url.test(/\?/)) url = url + '?' + data, data = null;
+		if (data && this.options.method == 'get') url = url + (url.test(/\?/) ? '?' : '&') + data, data = null;
 		(function(){
 			this.transport.open(this.options.method, url, this.options.async);
 			this.transport.onreadystatechange = this.onStateChange.bind(this);
