964fb863c3a2b61760ca45c7dc01f004fec33b4c
kamicane <kamicane@4db308c1-fb21-0410-9919-de62e267375e>
2007-04-29 17:46:34 +0000

    - Splitted Dom.js in Element.Selectors and Element.Filters. Removed Dom.js.  - modified scripts.json accordingly.
    git-svn-id: http://svn.mootools.net/trunk@523 4db308c1-fb21-0410-9919-de62e267375e

--- Element/Element.Selectors.js
+++ Element/Element.Selectors.js
@@ -1,6 +1,6 @@
 /*
-Script: Dom.js
-	Css Query related function and <Element> extensions
+Script: Element.Selectors.js
+	Css Query related functions and <Element> extensions
 
 License:
 	MIT-style license.
@@ -240,79 +240,3 @@ Element.extend({
 
 document.extend(Element.domMethods);
 Element.extend(Element.domMethods);
-
-/*
-Class: Elements
-	A collection of methods to be used with <$$> elements collections.
-*/
-
-Elements.extend({
-	
-	/*
-	Property: filterByTag
-		Filters the collection by a specified tag name.
-		Returns a new Elements collection, while the original remains untouched.
-	*/
-	
-	filterByTag: function(tag){
-		return this.filter(function(el){
-			return (el.tagName.toLowerCase() == tag);
-		});
-	},
-	
-	/*
-	Property: filterByClass
-		Filters the collection by a specified class name.
-		Returns a new Elements collection, while the original remains untouched.
-	*/
-	
-	filterByClass: function(className, nocash){
-		var elements = this.filter(function(el){
-			return el.className.contains(className, ' ');
-		});
-		return (nocash) ? elements : $extend(elements, new Elements);
-	},
-	
-	/*
-	Property: filterById
-		Filters the collection by a specified ID.
-		Returns a new Elements collection, while the original remains untouched.
-	*/
-	
-	filterById: function(id, nocash){
-		var elements = this.filter(function(el){
-			return (el.id == id);
-		});
-		return (nocash) ? elements : $extend(elements, new Elements);
-	},
-	
-	/*
-	Property: filterByAttribute
-		Filters the collection by a specified attribute.
-		Returns a new Elements collection, while the original remains untouched.
-		
-	Arguments:
-		name - the attribute name.
-		operator - optional, the attribute operator.
-		value - optional, the attribute value, only valid if the operator is specified.
-	*/
-	
-	filterByAttribute: function(name, operator, value, nocash){
-		var elements = this.filter(function(el){
-			var current = Element.prototype.getProperty.call(el, name);
-			if (!current) return false;
-			if (!operator) return true;
-			switch(operator){
-				case '=': return (current == value);
-				case '*=': return (current.contains(value));
-				case '^=': return (current.substr(0, value.length) == value);
-				case '$=': return (current.substr(current.length - value.length) == value);
-				case '!=': return (current != value);
-				case '~=': return current.contains(value, ' ');
-			}
-			return false;
-		});
-		return (nocash) ? elements : $extend(elements, new Elements);
-	}
-
-});
