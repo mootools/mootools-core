1da0352590b30834517eb8d993323166e06a9150
digitarald <digitarald@4db308c1-fb21-0410-9919-de62e267375e>
2007-05-02 22:03:10 +0000

    - fixed for broken packer
    git-svn-id: http://svn.mootools.net/trunk@547 4db308c1-fb21-0410-9919-de62e267375e

--- Effects/Fx.Transitions.js
+++ Effects/Fx.Transitions.js
@@ -4,7 +4,7 @@ Script: Fx.Transitions.js
 
 License:
 	MIT-style license.
-	
+
 Credits:
 	Easing Equations by Robert Penner, <http://www.robertpenner.com/easing/>, modified & optimized to be used with mootools.
 */
@@ -12,7 +12,7 @@ Credits:
 /*
 Class: Fx.Transitions
 	A collection of tweening transitions for use with the <Fx.Base> classes.
-	
+
 Example:
 	>//Elastic.easeOut with default values:
 	>new Fx.Style('margin', {transition: Fx.Transitions.Elastic.easeOut});
@@ -41,15 +41,15 @@ Fx.Transition = function(transition, params){
 };
 
 Fx.Transitions = new Abstract({
-	
+
 	/*
 	Property: linear
 		displays a linear transition.
-		
+
 	Graph:
 		(see Linear.png)
 	*/
-	
+
 	linear: function(p){
 		return p;
 	}
@@ -70,56 +70,56 @@ Fx.Transitions.compat = function(transition){
 };
 
 Fx.Transitions.extend({
-	
+
 	/*
 	Property: Quad
 		displays a quadratic transition. Must be used as Quad.easeIn or Quad.easeOut or Quad.easeInOut
-		
+
 	Graph:
 		(see Quad.png)
 	*/
-	
+
 	//auto generated
 
 	/*
 	Property: Cubic
 		displays a cubicular transition. Must be used as Cubic.easeIn or Cubic.easeOut or Cubic.easeInOut
-		
+
 	Graph:
 		(see Cubic.png)
 	*/
-	
+
 	//auto generated
 
 	/*
 	Property: Quart
 		displays a quartetic transition. Must be used as Quart.easeIn or Quart.easeOut or Quart.easeInOut
-		
+
 	Graph:
 		(see Quart.png)
 	*/
-	
+
 	//auto generated
-	
+
 	/*
 	Property: Quint
 		displays a quintic transition. Must be used as Quint.easeIn or Quint.easeOut or Quint.easeInOut
-		
+
 	Graph:
 		(see Quint.png)
 	*/
 
 	//auto generated
-	
+
 	/*
 	Property: Pow
 		Used to generate Quad, Cubic, Quart and Quint.
 		By default is p^6.
-		
+
 	Graph:
 		(see Pow.png)
 	*/
-	
+
 	Pow: function(p, x){
 		return Math.pow(p, x[0] || 6);
 	},
@@ -127,11 +127,11 @@ Fx.Transitions.extend({
 	/*
 	Property: Expo
 		displays a exponential transition. Must be used as Expo.easeIn or Expo.easeOut or Expo.easeInOut
-		
+
 	Graph:
 		(see Expo.png)
 	*/
-	
+
 	Expo: function(p){
 		return Math.pow(2, 8 * (p - 1));
 	},
@@ -139,24 +139,24 @@ Fx.Transitions.extend({
 	/*
 	Property: Circ
 		displays a circular transition. Must be used as Circ.easeIn or Circ.easeOut or Circ.easeInOut
-		
+
 	Graph:
 		(see Circ.png)
 	*/
-	
+
 	Circ: function(p){
 		return 1 - Math.sin(Math.acos(p));
 	},
-	
+
 
 	/*
 	Property: Sine
 		displays a sineousidal transition. Must be used as Sine.easeIn or Sine.easeOut or Sine.easeInOut
-		
+
 	Graph:
 		(see Sine.png)
 	*/
-	
+
 	Sine: function(p){
 		return 1 - Math.sin((1 - p) * Math.PI / 2);
 	},
@@ -164,7 +164,7 @@ Fx.Transitions.extend({
 	/*
 	Property: Back
 		makes the transition go back, then all forth. Must be used as Back.easeIn or Back.easeOut or Back.easeInOut
-		
+
 	Graph:
 		(see Back.png)
 	*/
@@ -177,25 +177,25 @@ Fx.Transitions.extend({
 	/*
 	Property: Bounce
 		makes the transition bouncy. Must be used as Bounce.easeIn or Bounce.easeOut or Bounce.easeInOut
-		
+
 	Graph:
 		(see Bounce.png)
 	*/
-	
+
 	Bounce: function(p){
-		for (var a = 0, b = 1; ; a += b, b /= 2){
-			if(p >= (7 - 4 * a) / 11) return - Math.pow((11 - 6 * a - 11 * p) / 4, 2) + b * b;
+		for (var a = 0, b = 1; 1; a += b, b /= 2){
+			if (p >= (7 - 4 * a) / 11) return - Math.pow((11 - 6 * a - 11 * p) / 4, 2) + b * b;
 		}
 	},
 
 	/*
 	Property: Elastic
 		Elastic curve. Must be used as Elastic.easeIn or Elastic.easeOut or Elastic.easeInOut
-		
+
 	Graph:
 		(see Elastic.png)
 	*/
-	
+
 	Elastic: function(p, x){
 		return Math.pow(2, 10 * --p) * Math.cos(20 * p * Math.PI * (x[0] || 1) / 3);
 	}
