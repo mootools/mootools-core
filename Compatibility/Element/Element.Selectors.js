91df6941e871d8409faa6393e37b27c134e6bc0d
digitarald <digitarald@4db308c1-fb21-0410-9919-de62e267375e>
2007-06-12 10:51:56 +0000

    - BREAKING CHANGE, removed $E and $ES, now available in the compatibility scripts
    git-svn-id: http://svn.mootools.net/trunk@650 4db308c1-fb21-0410-9919-de62e267375e

--- Compatibility/Element/Element.Selectors.js
+++ Compatibility/Element/Element.Selectors.js
@@ -1,7 +1,15 @@
 Element.$domCompatMethods = {
 	getElementsByClassName: function(className){
 		return this.getElements('.' + className);
-	}	
+	}
+};
+
+function $E(selector, filter){
+	return ($(filter) || document).getElement(selector);
+};
+
+function $ES(selector, filter){
+	return ($(filter) || document).getElementsBySelector(selector);
 };
 
 Element.extend(Element.$domCompatMethods);
