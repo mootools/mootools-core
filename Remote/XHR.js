aa4bb1342488dec825eaa5c431fa74348665edbf
digitarald <digitarald@4db308c1-fb21-0410-9919-de62e267375e>
2007-06-26 11:27:19 +0000

    - quick-fixed the buggy new-age-arguments-idea for Ajax and XHR
    git-svn-id: http://svn.mootools.net/trunk@705 4db308c1-fb21-0410-9919-de62e267375e

--- Remote/XHR.js
+++ Remote/XHR.js
@@ -126,7 +126,7 @@ var XHR = new Class({
 	*/
 
 	send: function(){
-		var params = $A(arguments).reverse().setKeys('data', 'url');
+		var params = $A(arguments).reverse().setKeys(['data', 'url']);
 		var url = params.url || this.url;
 		var data = params.data || this.options.data;
 		if (this.options.autoCancel) this.cancel();
