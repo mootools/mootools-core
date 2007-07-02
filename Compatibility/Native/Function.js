60d7ff63d4554cc5b27c75f9ee1211b46fe124b8
digitarald <digitarald@4db308c1-fb21-0410-9919-de62e267375e>
2007-07-02 10:33:57 +0000

    - added Function::bindWithEvent to compatibility script
    git-svn-id: http://svn.mootools.net/trunk@729 4db308c1-fb21-0410-9919-de62e267375e

--- Compatibility/Native/Function.js
+++ Compatibility/Native/Function.js
@@ -2,6 +2,10 @@ Function.extend({
 
 	bindAsEventListener: function(bind, args){
 		return this.create({'bind': bind, 'event': true, 'arguments': args});
+	},
+
+	bindWithEvent: function(bind, args){
+		return this.create({'bind': bind, 'event': true, 'arguments': args});
 	}
 
 });
