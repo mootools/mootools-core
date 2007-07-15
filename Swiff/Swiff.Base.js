b2c1610226122b96293d0a367c30ec426dcff595
digitarald <digitarald@4db308c1-fb21-0410-9919-de62e267375e>
2007-07-15 18:07:59 +0000

    - Work-in-progress: Added Swiff.Base and Swiff.Uploader ... Happy uploading
    git-svn-id: http://svn.mootools.net/trunk@762 4db308c1-fb21-0410-9919-de62e267375e

---
+++ Swiff/Swiff.Base.js
@@ -0,0 +1,173 @@
+/*
+Script: Swiff.Base.js
+	Contains <Swiff>, <Swiff.getVersion>, <Swiff.remote>
+
+Credits:
+	Flash detection 'borrowed' from SWFObject.
+
+License:
+	MIT-style license.
+*/
+
+/*
+Function: Swiff
+	creates a flash object with supplied parameters.
+
+Arguments:
+	moview - string; The path to the swf movie.
+	options - an object with options names as keys. See options below.
+
+Options:
+	width - (number) the width of the flash object. defaults to 1.
+	height - number) the height of the flash object. defaults to 1.
+	id - (string) the id of the flash object. defaults to 'SwiffX' (X is the Swiff UID).
+	inject - (element) the target container for the swf object
+	params - (object) object params (wmode, bgcolor, allowScriptAccess, loop, etc.), default: allowScriptAccess to sameDomain.
+	properties - (object) additional attributes for the object element.
+	vars - (object) given to the swf as querystring in flashVars.
+	callBacks - (object) Functions you want to pass to your flash movie.
+
+Returns:
+	The object element
+	Important: the $ function on the OBJECT element wont extend it, will just target the movie by its id/reference.
+	So its not possible to use the <Element> methods on it.
+
+Example:
+	(start code)
+	var obj = Swiff('myMovie.swf', {
+		inject: $('myElement')
+		width: 500,
+		height: 400,
+		id: 'myBeautifulMovie'
+		parameters: {
+			wmode: 'opaque',
+			bgcolor: '#ff3300',
+		},
+		vars: {
+			myVariable: myJsVar,
+			myVariableString: 'hello'
+		}
+		callBacks: {
+			onLoad: myOnloadFunc
+		}
+	});
+	(end)
+*/
+
+var Swiff = new Abstract(function(movie, options){
+	if (!Swiff.fixed) Swiff.fix();
+	options = $merge(Swiff.options, options || {});
+
+	var instance = Swiff.nextInstance();
+	var properties = $merge(options.properties, {
+		id: options.id || instance,
+		width: options.width,
+		height: options.height
+	});
+	var params = options.params;
+	var vars = options.vars;
+	Swiff.callBacks[instance] = {};
+	for (var prop in options.callBacks){
+		Swiff.callBacks[instance][prop] = options.callBacks[prop];
+		vars[prop] = 'Swiff.callBacks.' + instance + '.' + prop;
+	}
+	if ($type(vars) == 'object') vars = Object.toQueryString(vars);
+	if (vars) params.FlashVars = (params.FlashVars) ? (params.FlashVars + '&' + vars) : vars;
+
+	if (Client.Engine.ie){
+		properties.classid = 'clsid:D27CDB6E-AE6D-11cf-96B8-444553540000';
+		params.movie = movie;
+	} else{
+		properties.type = 'application/x-shockwave-flash';
+		properties.data = movie;
+	}
+
+	var build = '<object ';
+	for (var attr in properties) build += attr + '="' + properties[attr] + '" ';
+	build += '>';
+	for (var name in params) build += '<param name="' + name + '" value="' + params[name] + '" />';
+	build += '</object>';
+	var obj = new Element('div').setHTML(build).firstChild;
+	if (options.inject) $(options.inject).appendChild(obj);
+	return obj;
+});
+
+Swiff.extend({
+
+	options: {
+		width: 1,
+		height: 1,
+		id: null,
+		inject: null,
+		params: {
+			allowScriptAccess: 'sameDomain'
+		},
+		properties: {},
+		callBacks: {},
+		vars: {}
+	},
+
+	UID: 0,
+
+	callBacks: {},
+
+	nextInstance: function(){
+		return 'Swiff' + (++Swiff.UID);
+	},
+
+	//from swfObject, fixes bugs in ie+fp9
+	fix: function(){
+		Swiff.fixed = true;
+		window.addEvent('beforeunload', function(){
+			__flash_unloadHandler = __flash_savedUnloadHandler = Class.empty;
+		});
+		if (!Client.Engine.ie) return;
+		window.addEvent('unload', function(){
+			Array.each(document.getElementsByTagName('object'), function(swf){
+				swf.style.display = 'none';
+				for (var p in swf){
+					if (typeof swf[p] == 'function') swf[p] = Class.empty;
+				}
+			});
+		});
+	},
+
+	/*
+	Function: Swiff.getVersion
+		Gets the major version of the flash player installed.
+
+	Returns:
+		A number representing the (major) flash version installed, or 0 if no player is installed.
+	*/
+
+	getVersion: function(){
+		if (!$defined(Swiff.pluginVersion)){
+			var x;
+			if (navigator.plugins && navigator.mimeTypes.length){
+				x = navigator.plugins["Shockwave Flash"];
+				if(x && x.description) x = x.description;
+			} else if (Client.Engine.ie){
+				try {
+					x = new ActiveXObject("ShockwaveFlash.ShockwaveFlash");
+					x = x.GetVariable("$version");
+				} catch(e){}
+			}
+			Swiff.pluginVersion = ($type(x) == 'string') ? parseInt(x.match(/\d+/)[0]) : 0;
+		}
+		return Swiff.pluginVersion;
+	},
+
+	/*
+	Function: Swiff.remote
+		Calls an ActionScript function from javascript. Requires ExternalInterface.
+
+	Returns:
+		Whatever the ActionScript Returns
+	*/
+
+	remote: function(obj, fn){
+		var rs = obj.CallFunction('<invoke name="' + fn + '" returntype="javascript">' + __flash__argumentsToXML(arguments, 2) + '</invoke>');
+		return eval(rs);
+	}
+
+});
---
+++ Swiff/Swiff.Uploader.js
@@ -0,0 +1,168 @@
+/*
+Script: Swiff.Uploader.js
+	Contains <Swiff.Uploader>
+
+License:
+	MIT-style license.
+*/
+
+/*
+Class: Swiff.Uploader
+	Creates an uploader instance.
+	All instances use the first injected swf instance
+
+Arguments:
+	callBacks - an object, containing key/value pairs, representing the possible callbacks. See below.
+	onLoaded - Callback when the swf is initialized
+	options - types, multiple, queued, swf, url, container
+
+Options:
+	types - (object) description/filter pairs for file browser (e.g. {'Images (*.jpg, *.jpeg, *.gif, *.png)': '*.jpg; *.jpeg; *.gif; *.png'} ), default false
+	multiple - (boolean) Allow upload of multiple files, default true.
+	queued - (boolean) Queued start for multiple upload, default true.
+	swf - (string) URL to the swf file, default 'Swf.Uploader.swf'.
+	url - (string) default upload URL, can be overridden in upload. Absolute URLs are recommended.
+
+callBacks:
+	onOpen - a function to fire when the user opens a file.
+	onProgress - a function to fire when the file is uploading. passes the name, the current uploaded size and the full size.
+	onSelect - a function to fire when the user selects a file.
+	onComplete - a function to fire when the file finishes uploading
+	onError - a function to fire when there is an error.
+	onCancel - a function to fire when the user cancels the file uploading.
+*/
+
+Swiff.Uploader = new Class({
+
+	options: {
+		types: false,
+		multiple: true,
+		queued: true,
+		swf: 'Swf.Uploader.swf',
+		url: null,
+		container: null
+	},
+
+	callBacks: {
+		onOpen: $empty,
+		onProgress: $empty,
+		onSelect: $empty,
+		onComplete: $empty,
+		onError: $empty,
+		onCancel: $empty
+	},
+
+	initialize: function(callBacks, onLoad, options){
+		if (Swiff.getVersion() < 8) return false;
+		this.setOptions(options);
+		this.onLoad = onLoad;
+		callBacks = $merge(this.callBacks, callBacks || {});
+		for (p in callBacks) callBacks[p] = callBacks[p].bind(this);
+		this.instance = Swiff.nextInstance();
+		Swiff.callBacks[this.instance] = callBacks;
+		this.object = Swiff.Uploader.register(this.loaded.bind(this), this.options.swf, this.options.container);
+		return this;
+	},
+
+	loaded: function(){
+		Swiff.remote(this.object, 'create', this.instance, this.options.types, this.options.multiple, this.options.queued, this.options.url);
+		this.onLoad.delay(10, this);
+	},
+
+	/*
+	Property: browse
+		Open the file browser.
+	*/
+
+	browse: function(){
+		Swiff.remote(this.object, 'browse', this.instance);
+	},
+
+	/*
+	Property: send
+		Starts the upload of all selected files.
+
+	Arguments:
+		url - (string, optional) Upload URL, sets URL also for all following uploads.
+
+	*/
+	send: function(url){
+		Swiff.remote(this.object, 'upload', this.instance, url);
+	},
+
+	/*
+	Property: remove
+		For multiple uploads cancels and removes the given file from queue.
+
+	Arguments:
+		name - (string) Filename
+		name - (string) Filesize in byte
+	*/
+	remove: function(name, size){
+		Swiff.remote(this.object, 'remove', this.instance, name, size);
+	},
+
+	/*
+	Property: fileIndex
+		Returns the internal index of the given file
+
+	Arguments:
+		name - (string) Filename
+		name - (string) Filesize in byte
+
+	Returns:
+		(number) The index of the file or -1 when the file is not found.
+
+	*/
+	fileIndex: function(name, size){
+		return Swiff.remote(this.object, 'fileIndex', this.instance, name, size);
+	},
+
+	/*
+	Property: fileList
+		Returns one Array with with arrays containing name and size of the file.
+
+	Returns:
+		(array) An array with [name, size] items
+	*/
+	fileList: function(){
+		return Swiff.remote(this.object, 'filelist', this.instance);
+	}
+
+});
+
+Swiff.Uploader.implement(new Options);
+
+Swiff.Uploader.factory = $extend;
+
+Swiff.Uploader.factory({
+
+	onLoads: [],
+
+	register: function(onLoad, movie, inject){
+		if (!this.object || !this.loaded) {
+			this.onLoads.push(onLoad);
+			if (!this.object) {
+				this.object = new Swiff(movie, {
+					callBacks: {
+						'onLoad': this.onLoad.create({create: 10, bind: this})
+					},
+					params: {
+						wmode: 'transparent'
+					},
+					inject: $(inject) || document.body
+				});
+			}
+		} else onLoad.delay(10);
+		return this.object;
+	},
+
+	onLoad: function(val){
+		this.loaded = true;
+		this.onLoads.each(function(fn){
+			fn();
+		});
+		this.onLoads.empty();
+	}
+
+});
