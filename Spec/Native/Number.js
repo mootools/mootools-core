3defcef37fa30a8900fc2d51412de0754413012a
ibolmo <ibolmo@4db308c1-fb21-0410-9919-de62e267375e>
2007-10-07 05:13:54 +0000

    Spec/Native/Number.js && Spec/Native/String.js  - Cleaned up the spec style and added some more examples.  - Removed *.Generic (again, can be assumed to work because of Native specs
    git-svn-id: http://svn.mootools.net/trunk@1015 4db308c1-fb21-0410-9919-de62e267375e

--- Spec/Native/Number.js
+++ Spec/Native/Number.js
@@ -8,27 +8,37 @@ License:
 
 describe('Number', {
 
-	toInt: function(){
+	'should convert a number `toInt`': function(){
 		value_of((111).toInt()).should_be(111);
+	},
+
+	'should convert a number depending on the radix': function(){
 		value_of((111).toInt(2)).should_be(7);
 		value_of((0x16).toInt(10)).should_be(22); //ECMA standard, radix is optional so if starts with 0x then parsed as hexadecimal
 		value_of((016).toInt(10)).should_be(14); //ECMA standard, radix is optional so if starts with 0 then parsed as octal
 	},
 
-	toFloat: function(){
+	'should convert a number `toFloat`': function(){
 		value_of((1.00).toFloat()).should_be(1);
 		value_of((1.12 - 0.12).toFloat()).should_be(1);
 		value_of((0.0010).toFloat()).should_be(0.001);
 		value_of((Number.MIN_VALUE).toFloat()).should_be(Number.MIN_VALUE);
 	},
 
-	limit: function(){
+	'should `limit` a number within a range': function(){
 		value_of((-1).limit(0, 1)).should_be(0);
 		value_of((3).limit(1, 2)).should_be(2);
 	},
 
-	round: function(){
+	'should not `limit` a number if within the range': function(){
+		value_of((2).limit(0,4)).should_be(2);
+	},
+
+	'should `round` a number to the nearest whole number if units place is not specified': function(){
 		value_of((0.01).round()).should_be(0);
+	},
+
+	'should `round` a number according the units place specified': function(){
 		value_of((0.01).round(2)).should_be(0.01);
 		value_of((1).round(3)).should_be(1);
 		value_of((-1.01).round()).should_be(-1);
@@ -38,7 +48,7 @@ describe('Number', {
 		value_of((100).round(-5)).should_be(0);
 	},
 
-	times: function(){
+	'should call the function for the specified number of `times`': function(){
 		var found = 0;
 		(3).times(function(i){
 			found = i;
@@ -49,70 +59,16 @@ describe('Number', {
 			found2 = i;
 		});
 
+		value_of(found).should_be(2);
+		value_of(found2).should_be(-1);
+	},
+
+	'should bind and call the function for the specified number of `times`': function(){
 		var aTest = 'hi';
 		var found3 = false;
 		(1).times(function(i){
 			found3 = (this == aTest);
 		}, aTest);
-
-		value_of(found).should_be(2);
-		value_of(found2).should_be(-1);
-		value_of(found3).should_be_true();
-	}
-
-});
-
-describe('Number Generics', {
-
-	toInt: function(){
-		value_of(Number.toInt(111)).should_be(111);
-		value_of(Number.toInt(111, 2)).should_be(7);
-		value_of(Number.toInt(0x16, 10)).should_be(22); //ECMA standard, radix is optional so if starts with 0x then parsed as hexadecimal
-		value_of(Number.toInt(016, 10)).should_be(14); //ECMA standard, radix is optional so if starts with 0 then parsed as octal
-	},
-
-	toFloat: function(){
-		value_of(Number.toFloat(1.00)).should_be(1);
-		value_of(Number.toFloat(1.12 - 0.12)).should_be(1);
-		value_of(Number.toFloat(0.0010)).should_be(0.001);
-		value_of(Number.toFloat(Number.MIN_VALUE)).should_be(Number.MIN_VALUE);
-	},
-
-	limit: function(){
-		value_of(Number.limit(-1, 0, 1)).should_be(0);
-		value_of(Number.limit(3, 1, 2)).should_be(2);
-	},
-
-	round: function(){
-		value_of(Number.round(0.01)).should_be(0);
-		value_of(Number.round(0.01, 2)).should_be(0.01);
-		value_of(Number.round(1, 3)).should_be(1);
-		value_of(Number.round(-1.01)).should_be(-1);
-		value_of(Number.round(-1.01, 2)).should_be(-1.01);
-		value_of(Number.round(111, -1)).should_be(110);
-		value_of(Number.round(-111, -2)).should_be(-100);
-		value_of(Number.round(100, -5)).should_be(0);
-	},
-
-	times: function(){
-		var found = 0;
-		Number.times(3, function(i){
-			found = i;
-		});
-
-		var found2 = -1;
-		Number.times(0, function(i){
-			found2 = i;
-		});
-
-		var aTest = 'hi';
-		var found3 = false;
-		Number.times(1, function(i){
-			found3 = (this == aTest);
-		}, aTest);
-
-		value_of(found).should_be(2);
-		value_of(found2).should_be(-1);
 		value_of(found3).should_be_true();
 	}
 
--- Spec/Native/String.js
+++ Spec/Native/String.js
@@ -8,138 +8,81 @@ License:
 
 describe('String', {
 
-	capitalize: function(){
+	'should `capitalize` each word': function(){
 		value_of('i like cookies'.capitalize()).should_be('I Like Cookies');
 		value_of('I Like cOOKIES'.capitalize()).should_be('I Like COOKIES');
 	},
 
-	camelCase: function(){
+	'should convert a hyphenated string into a `camelCase`d string': function(){
 		value_of('i-like-cookies'.camelCase()).should_be('iLikeCookies');
 		value_of('I-Like-Cookies'.camelCase()).should_be('ILikeCookies');
 	},
 
-	hyphenate: function(){
+	'should convert a camelCased string into a `hyphenate`d string': function(){
 		value_of('iLikeCookies'.hyphenate()).should_be('i-like-cookies');
 		value_of('ILikeCookies'.hyphenate()).should_be('-i-like-cookies');
 	},
 
-	clean: function(){
+	'should `clean` all extraneous whitespace from the string': function(){
 		value_of('  i     like    cookies   '.clean()).should_be("i like cookies");
 		value_of('  i \n like \n cookies \n\t  '.clean()).should_be("i like cookies");
 	},
 
-	trim: function(){
+	'should `trim` left and right whitespace from the string': function(){
 		value_of('  i like cookies  '.trim()).should_be('i like cookies');
 		value_of('  i  like  cookies  '.trim()).should_be('i  like  cookies');
 	},
 
-	contains: function(){
+	'should return true if the string `contains` the string, otherwise false': function(){
 		value_of('i like cookies'.contains('cookies')).should_be_true();
+		value_of('i,like,cookies'.contains('cookies')).should_be_true();
+		value_of('mootools'.contains('inefficient javascript')).should_be_false();
+	},
+
+	'should return true if the string `constains` the string and separator, otherwise false': function(){
 		value_of('i like cookies'.contains('cookies', ' ')).should_be_true();
 		value_of('i like cookies'.contains('cookies', ',')).should_be_false();
-		value_of('i,like,cookies'.contains('cookies')).should_be_true();
+
 		value_of('i,like,cookies'.contains('cookies', ' ')).should_be_false();
 		value_of('i,like,cookies'.contains('cookies', ',')).should_be_true();
 	},
 
-	test: function(){
+	'should return true if the `test` matches the string, otherwise false': function(){
 		value_of('i like teh cookies'.test('cookies')).should_be_true();
 		value_of('i like cookies'.test('ke coo')).should_be_true();
+		value_of('I LIKE COOKIES'.test('cookie', 'i')).should_be_true();
 		value_of('i like cookies'.test('cookiez')).should_be_false();
+	},
+
+	'should return true if the regular expression `test` matches the string, otherwise false': function(){
 		value_of('i like cookies'.test(/like/)).should_be_true();
 		value_of('i like cookies'.test(/^l/)).should_be_false();
 	},
 
-	toInt: function(){
+	'should `toInt` the string': function(){
 		value_of('10'.toInt()).should_be(10);
 		value_of('10px'.toInt()).should_be(10);
 		value_of('10.10em'.toInt()).should_be(10);
+	},
+
+	'should `toInt` the string and change the base': function(){
 		value_of('10'.toInt(5)).should_be(5);
 	},
 
-	toFloat: function(){
+	'should `toFloat` the string': function(){
 		value_of('10.11'.toFloat()).should_be(10.11);
 		value_of('10.55px'.toFloat()).should_be(10.55);
 	},
 
-	rgbToHex: function(){
+	'should `rgbToHex` a CSS rgb string into a CSS hex string': function(){
 		value_of('rgb(255,255,255)'.rgbToHex()).should_be('#ffffff');
 		value_of('rgb(255,255,255,0)'.rgbToHex()).should_be('transparent');
 	},
 
-	hexToRgb: function(){
+	'should `hexToRgb` a CSS hex string into a CSS rgb string': function(){
 		value_of('#fff'.hexToRgb()).should_be('rgb(255,255,255)');
 		value_of('ff00'.hexToRgb()).should_be('rgb(255,0,0)');
 		value_of('#000000'.hexToRgb()).should_be('rgb(0,0,0)');
-	}
-
-});
-
-describe('String Generics', {
-
-	capitalize: function(){
-		value_of(String.capitalize('i like cookies')).should_be('I Like Cookies');
-		value_of(String.capitalize('I Like cOOKIES')).should_be('I Like COOKIES');
-	},
-
-	camelCase: function(){
-		value_of(String.camelCase('i-like-cookies')).should_be('iLikeCookies');
-		value_of(String.camelCase('I-Like-Cookies')).should_be('ILikeCookies');
-	},
-
-	hyphenate: function(){
-		value_of(String.hyphenate('iLikeCookies')).should_be('i-like-cookies');
-		value_of(String.hyphenate('ILikeCookies')).should_be('-i-like-cookies');
-	},
-
-	clean: function(){
-		value_of(String.clean('  i     like    cookies   ')).should_be("i like cookies");
-		value_of(String.clean('  i \n like \n cookies \n\t  ')).should_be("i like cookies");
-	},
-
-	trim: function(){
-		value_of(String.trim('  i like cookies  ')).should_be('i like cookies');
-		value_of(String.trim('  i  like  cookies  ')).should_be('i  like  cookies');
-	},
-
-	contains: function(){
-		value_of(String.contains('i like cookies', 'cookies')).should_be_true();
-		value_of(String.contains('i like cookies', 'cookies', ' ')).should_be_true();
-		value_of(String.contains('i like cookies', 'cookies', ',')).should_be_false();
-		value_of(String.contains('i,like,cookies', 'cookies')).should_be_true();
-		value_of(String.contains('i,like,cookies', 'cookies', ' ')).should_be_false();
-		value_of(String.contains('i,like,cookies', 'cookies', ',')).should_be_true();
-	},
-
-	test: function(){
-		value_of(String.test('i like teh cookies', 'cookies')).should_be_true();
-		value_of(String.test('i like cookies', 'ke coo')).should_be_true();
-		value_of(String.test('i like cookies', 'cookiez')).should_be_false();
-		value_of(String.test('i like cookies', /like/)).should_be_true();
-		value_of(String.test('i like cookies', /^l/)).should_be_false();
-	},
-
-	toInt: function(){
-		value_of(String.toInt('10')).should_be(10);
-		value_of(String.toInt('10px')).should_be(10);
-		value_of(String.toInt('10.10em')).should_be(10);
-		value_of(String.toInt('10', 5)).should_be(5);
-	},
-
-	toFloat: function(){
-		value_of(String.toFloat('10.11')).should_be(10.11);
-		value_of(String.toFloat('10.55px')).should_be(10.55);
-	},
-
-	rgbToHex: function(){
-		value_of(String.rgbToHex('rgb(255,255,255)')).should_be('#ffffff');
-		value_of(String.rgbToHex('rgb(255,255,255,0)')).should_be('transparent');
-	},
-
-	hexToRgb: function(){
-		value_of(String.hexToRgb('#fff')).should_be('rgb(255,255,255)');
-		value_of(String.hexToRgb('ff00')).should_be('rgb(255,0,0)');
-		value_of(String.hexToRgb('#000000')).should_be('rgb(0,0,0)');
 	}
 
 });
