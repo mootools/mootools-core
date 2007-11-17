bf9338d2c0fd3b4caa341059e585a2ccfbd058d0
kamicane <kamicane@4db308c1-fb21-0410-9919-de62e267375e>
2007-11-17 13:51:34 +0000

    - fixed outdated code in SmoothScroll
    git-svn-id: http://svn.mootools.net/trunk@1254 4db308c1-fb21-0410-9919-de62e267375e

--- Source/Plugins/SmoothScroll.js
+++ Source/Plugins/SmoothScroll.js
@@ -13,17 +13,18 @@ var SmoothScroll = new Class({
 
 	Extends: Fx.Scroll,
 
-	initialize: function(options, win){
-		arguments.callee.parent(win || window, options);
-		this.links = (this.options.links) ? $$(this.options.links) : $$(this.element.document.links);
-		var location = this.element.location.href.match(/^[^#]*/)[0] + '#';
+	initialize: function(options, doc){
+		doc = doc || document;
+		arguments.callee.parent(doc, options);
+		this.links = (this.options.links) ? $$(this.options.links) : $$(doc.links);
+		var location = doc.window.location.href.match(/^[^#]*/)[0] + '#';
 		this.links.each(function(link){
 			if (link.href.indexOf(location) != 0) return;
 			var anchor = link.href.substr(location.length);
 			if (anchor && $(anchor)) this.useLink(link, anchor);
 		}, this);
 		if (!Browser.Engine.webkit419) this.addEvent('onComplete', function(){
-			this.element.location.hash = this.anchor;
+			doc.window.location.hash = this.anchor;
 		}, true);
 	},
 
