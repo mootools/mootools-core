5dd4e578755d921c9cc78ba5f1e123fc68ffe3e9
kamicane <kamicane@4db308c1-fb21-0410-9919-de62e267375e>
2007-12-20 16:52:16 +0000

    - separated the Event Native from Element.Event.
    git-svn-id: http://svn.mootools.net/trunk@1316 4db308c1-fb21-0410-9919-de62e267375e

--- Source/Element/Element.Event.js
+++ Source/Element/Element.Event.js
@@ -1,101 +1,10 @@
 /*
 Script: Element.Event.js
-	Contains the Event Native, Element methods for dealing with events, and custom Events.
+	Contains Element methods for dealing with events, and custom Events.
 
 License:
 	MIT-style license.
 */
-
-var Event = new Native({
-
-	name: 'Event',
-
-	initialize: function(event, win){
-		win = win || window;
-		event = event || win.event;
-		if (event.$extended) return event;
-		this.$extended = true;
-		var type = event.type;
-		var target = event.target || event.srcElement;
-		while (target && target.nodeType == 3) target = target.parentNode;
-		if (type.test(/DOMMouseScroll|mousewheel/)){
-			this.wheel = (event.wheelDelta) ? event.wheelDelta / 120 : -(event.detail || 0) / 3;
-		} else if (type.test(/key/)){
-			this.code = event.which || event.keyCode;
-			var key = Event.Keys.keyOf(this.code);
-			if (type == 'keydown'){
-				var fKey = this.code - 111;
-				if (fKey > 0 && fKey < 13) key = 'f' + fKey;
-			}
-			this.key = key || String.fromCharCode(this.code).toLowerCase();
-		} else if (type.test(/(click|mouse|menu)/)){
-			this.page = {
-				x: event.pageX || event.clientX + win.document.documentElement.scrollLeft,
-				y: event.pageY || event.clientY + win.document.documentElement.scrollTop
-			};
-			this.client = {
-				x: event.pageX ? event.pageX - win.pageXOffset : event.clientX,
-				y: event.pageY ? event.pageY - win.pageYOffset : event.clientY
-			};
-			this.rightClick = (event.which == 3) || (event.button == 2);
-			var related = null;
-			if (type.test(/over|out/)){
-				switch (type){
-					case 'mouseover': related = event.relatedTarget || event.fromElement; break;
-					case 'mouseout': related = event.relatedTarget || event.toElement;
-				}
-				if ((function(){
-					while (related && related.nodeType == 3) related = related.parentNode;
-				}).create({attempt: Browser.Engine.gecko})() === false) related = false;
-			}
-		}
-
-		return $extend(this, {
-			event: event,
-			type: type,
-			relatedTarget: related,
-			target: target,
-			shift: event.shiftKey,
-			control: event.ctrlKey,
-			alt: event.altKey,
-			meta: event.metaKey
-		});
-	}
-
-});
-
-Event.Keys = new Hash({
-	'enter': 13,
-	'up': 38,
-	'down': 40,
-	'left': 37,
-	'right': 39,
-	'esc': 27,
-	'space': 32,
-	'backspace': 8,
-	'tab': 9,
-	'delete': 46
-});
-
-Event.implement({
-
-	stop: function(){
-		return this.stopPropagation().preventDefault();
-	},
-
-	stopPropagation: function(){
-		if (this.event.stopPropagation) this.event.stopPropagation();
-		else this.event.cancelBubble = true;
-		return this;
-	},
-
-	preventDefault: function(){
-		if (this.event.preventDefault) this.event.preventDefault();
-		else this.event.returnValue = false;
-		return this;
-	}
-
-});
 
 Element.Properties.events = {set: function(events){
 	this.addEvents(events);
