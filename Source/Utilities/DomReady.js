f894a59d2068cbd058853732805048b9d823a10c
kamicane <kamicane@4db308c1-fb21-0410-9919-de62e267375e>
2008-02-08 21:36:28 +0000

    - applied this code (http://javascript.nwbox.com/IEContentLoaded/) and (http://www.hedgerwow.com/360/dhtml/ie-dom-ondocumentready.html) to domReady in explorer ("doScroll" technique). Works fine in every test I made, seems solid. fixes #443.  - DomReady code was also highly optimized. Now smaller and faster, gets executed only once per pageload.
    git-svn-id: http://svn.mootools.net/trunk@1364 4db308c1-fb21-0410-9919-de62e267375e

--- Source/Utilities/DomReady.js
+++ Source/Utilities/DomReady.js
@@ -9,54 +9,41 @@ License:
 Element.Events.domready = {
 
 	onAdd: function(fn){
-		
-		if (Browser.loaded) return fn.call(this);
-		
-		var self = this, win = this.getWindow(), doc = this.getDocument();
-		
-		var domready = function(){
-			if (!arguments.callee.done){
-				arguments.callee.done = true;
-				fn.call(self);
-			};
-			return true;
-		};
-		
-		var states = (Browser.Engine.webkit) ? ['loaded', 'complete'] : 'complete';
-		
-		var check = function(context){
-			if (states.contains(context.readyState)) return domready();
-			return false;
-		};
-		
-		if (doc.readyState && Browser.Engine.webkit){
-			
-			(function(){
-				if (!check(doc)) arguments.callee.delay(50);
-			})();
-			
-		} else if (doc.readyState && Browser.Engine.trident){
-			
-			var script = $('ie_domready');
-			if (!script){
-				var src = (win.location.protocol == 'https:') ? '//:' : 'javascript:void(0)';
-				doc.write('<script id="ie_domready" defer src="' + src + '"></script>');
-				script = $('ie_domready');
-			}
-			if (!check(script)) script.addEvent('readystatechange', check.pass(script));
-			
-		} else {
-			
-			win.addEvent('load', domready);
-			doc.addEvent('DOMContentLoaded', domready);
-			
-		}
-		
-		return null;
+		if (Browser.loaded) fn.call(this);
 	}
 
 };
 
-window.addEvent('domready', function(){
-	Browser.loaded = true;
-});
+(function(){
+	
+	var domready = function(){
+		if (Browser.loaded) return;
+		Browser.loaded = true;
+		window.fireEvent('domready');
+		document.fireEvent('domready');
+	};
+	
+	switch (Browser.Engine.name){
+
+		case 'webkit': (function(){
+			(['loaded', 'complete'].contains(document.readyState)) ? domready() : arguments.callee.delay(50);
+		})(); break;
+
+		case 'trident': (function(){
+			var temp = document.createElement('div');
+			try {
+				temp.doScroll('left');
+			} catch(e){
+				arguments.callee.delay(50);
+			} finally {
+				domready();
+			}
+		})(); break;
+		
+		default:
+			window.addEvent('load', domready);
+			document.addEvent('DOMContentLoaded', domready);
+
+	}
+	
+})();
