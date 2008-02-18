17f5d8ac141aa989ca08242676e149118e9330dd
kamicane <kamicane@4db308c1-fb21-0410-9919-de62e267375e>
2008-02-18 01:44:34 +0000

    - updated Selectors.Children to work with the `new` pseudo selectors API.
    git-svn-id: http://svn.mootools.net/trunk@1405 4db308c1-fb21-0410-9919-de62e267375e

--- Source/Plugins/Selectors.Children.js
+++ Source/Plugins/Selectors.Children.js
@@ -6,51 +6,35 @@ License:
 	MIT-style license.
 */
 
-Selectors.Pseudo.children = {
+Selectors.Cache.children = {};
 
-	parser: function(argument){
-		argument = (argument) ? argument.match(/^([-+]?\d*)?([\-+:])?([-+]?\d*)?$/) : [null, 0, false, 0];
-		if (!argument) return false;
-		argument[1] = parseInt(argument[1]) || 0;
-		var int1 = parseInt(argument[3]);
-		argument[3] = ($chk(int1)) ? int1 : 0;
-		switch (argument[2]){
-			case '-': case '+': case ':': return {'a': argument[1], 'b': argument[3], 'special': argument[2]};
-			default: return {'a': argument[1], 'b': 0, 'special': 'index'};
-		}
-	},
+Selectors.Utils.parseChildrenArgument = function(argument){
+	
+	if (Selectors.Cache.children[argument]) return Selectors.Cache.children[argument];
+	
+	argument = (argument) ? argument.match(/^([-+]?\d*)?([\-+:])?([-+]?\d*)?$/) : [null, 0, false, 0];
+	if (!argument) return false;
+	argument[1] = parseInt(argument[1]) || 0;
+	var int1 = parseInt(argument[3]);
+	argument[3] = ($chk(int1)) ? int1 : 0;
+	var parsed;
+	switch (argument[2]){
+		case '-': case '+': case ':': parsed = {a: argument[1], b: argument[3], special: argument[2]}; break;
+		default: parsed = {a: argument[1], b: 0, special: 'index'};
+	}
+	return Selectors.Cache.children[argument] = parsed;
+};
 
-	xpath: function(argument){
-		var include = '';
-		var len = 'count(../child::*)';
-		var a = argument.a + ' + ' + ((argument.a < 0) ? len : 0);
-		var b = argument.b + ' + ' + ((argument.b < 0) ? len : 0);
-		var pos = 'position()';
-		switch (argument.special){
-			case '-':
-				b = '((' + a + ' - ' + b + ') mod (' + len + '))';
-				a += ' + 1';
-				b += ' + 1';
-				include = '(' + b + ' < 1 and (' + pos + ' <= ' + a + ' or ' + pos + ' >= (' + b + ' + ' + len + ')' + ')) or (' + pos + ' <= ' + a + ' and ' + pos + ' >= ' + b + ')';
-			break;
-			case '+': b = '((' + a + ' + ' + b + ') mod ( ' + len + '))';
-			case ':':
-				a += ' + 1';
-				b += ' + 1';
-				include = '(' + b + ' < ' + a + ' and (' + pos + ' >= ' + a + ' or ' + pos + ' <= ' + b + ')) or (' + pos + ' >= ' + a + ' and ' + pos + ' <= ' + b + ')';
-			break;
-			default: include = (a + ' + 1');
-		}
-		return '[' + include + ']';
-	},
-
-	filter: function(argument, Local){
-		Local.i = Local.i || 0;
-		Local.all = Local.all || this.parentNode.childNodes;
-		Local.len = Local.len || Local.all.length;
-		var i = Local.i;
-		var len = Local.len;
-		var all = Local.all;
+Selectors.Pseudo.extend({
+	
+	children: function(argument, local){
+		argument = Selectors.Utils.parseChildrenArgument(argument);		
+		local.i = local.i || 0;
+		local.all = local.all || this.parentNode.childNodes;
+		local.len = local.len || local.all.length;
+		var i = local.i;
+		var len = local.len;
+		var all = local.all;
 		var include = false;
 		var a = argument.a + ((argument.a < 0) ? len : 0);
 		var b = argument.b + ((argument.b < 0) ? len : 0);
@@ -63,7 +47,8 @@ Selectors.Pseudo.children = {
 			case ':': include = (b < a) ? (i >= a || i <= b) : (i >= a && i <= b); break;
 			default: include = (all[a] == this);
 		}
-		Local.i++;
-		return include;
+		local.i++;
+		return include;	
 	}
-};
+	
+});
