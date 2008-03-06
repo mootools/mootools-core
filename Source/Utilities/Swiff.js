1e8e1863942e50b148f5e13984d5f904045ab62c
kamicane <kamicane@4db308c1-fb21-0410-9919-de62e267375e>
2008-03-06 02:33:54 +0000

    - Swiff is now a Class, to allow direct injection / replace of the Swiff instance.  - every instance has now its remove method.  - every instance has a toElement method, so $(mySwiff) will return the object.
    git-svn-id: http://svn.mootools.net/trunk@1468 4db308c1-fb21-0410-9919-de62e267375e

--- Source/Utilities/Swiff.js
+++ Source/Utilities/Swiff.js
@@ -9,10 +9,12 @@ Credits:
 	Flash detection & Internet Explorer + Flash Player 9 fix inspired by SWFObject.
 */
 
-var Swiff = function(path, options){
-	var instance = 'Swiff_' + $time();
-	options = $merge({
-		id: instance,
+var Swiff = new Class({
+	
+	Implements: [Options],
+	
+	options: {
+		id: null,
 		height: 1,
 		width: 1,
 		container: null,
@@ -25,33 +27,72 @@ var Swiff = function(path, options){
 		},
 		events: {},
 		vars: {}
-	}, options);
-	var params = options.params, vars = options.vars, id = options.id;
-	var properties = $extend({height: options.height, width: options.width}, options.properties);
-	Swiff.Events[instance] = {};
-	for (var event in options.events){
-		Swiff.Events[instance][event] = (function(option){
-			return function(){
-				option.apply($(options.id), arguments);
-			};
-		})(options.events[event]);
-		vars[event] = 'Swiff.Events.' + instance + '.' + event;
+	},
+	
+	toElement: function(){
+		return this.object;
+	},
+	
+	initialize: function(path, options){
+		this.instance = 'Swiff_' + $time();
+		
+		this.setOptions(options);
+		options = this.options;
+		var id = this.id = options.id || this.instance;
+		var container = $(options.container);
+
+		Swiff.Events[this.instance] = {};
+		
+		var params = options.params, vars = options.vars, events = options.events;
+		var properties = $extend({height: options.height, width: options.width}, options.properties);
+		
+		for (var option in options){
+			if ((/^on[A-Z]/).test(option)) events[option] = options[option];
+		}
+		
+		var self = this;
+		
+		for (var event in events){
+			Swiff.Events[this.instance][event] = (function(option){
+				return function(){
+					option.apply(self.object, arguments);
+				};
+			})(events[event]);
+			vars[event] = 'Swiff.Events.' + this.instance + '.' + event;
+		}
+		
+		params.flashVars = Hash.toQueryString(vars);
+		if (Browser.Engine.trident){
+			properties.classid = 'clsid:D27CDB6E-AE6D-11cf-96B8-444553540000';
+			params.movie = path;
+		} else {
+			properties.type = 'application/x-shockwave-flash';
+			properties.data = path;
+		}
+		var build = '<object id="' + id + '"';
+		for (var property in properties) build += ' ' + property + '="' + properties[property] + '"';
+		build += '>';
+		for (var param in params) build += '<param name="' + param + '" value="' + params[param] + '" />';
+		build += '</object>';
+		this.object = (container || new Element('div')).set('html', build).firstChild;
+	},
+	
+	replaces: function(element){
+		element = $(element, true);
+		element.parentNode.replaceChild(this.getElement(), element);
+		return this;
+	},
+	
+	inject: function(element){
+		$(element, true).appendChild(this.getElement());
+		return this;
+	},
+	
+	remote: function(fn){
+		return Swiff.remote(this.getElement(), fn);
 	}
-	params.flashVars = Hash.toQueryString(vars);
-	if (Browser.Engine.trident){
-		properties.classid = 'clsid:D27CDB6E-AE6D-11cf-96B8-444553540000';
-		params.movie = path;
-	} else {
-		properties.type = 'application/x-shockwave-flash';
-		properties.data = path;
-	}
-	var build = '<object id="' + options.id + '"';
-	for (var property in properties) build += ' ' + property + '="' + properties[property] + '"';
-	build += '>';
-	for (var param in params) build += '<param name="' + param + '" value="' + params[param] + '" />';
-	build += '</object>';
-	return ($(options.container) || new Element('div')).set('html', build).firstChild;
-};
+	
+});
 
 Swiff.Events = {};
 
