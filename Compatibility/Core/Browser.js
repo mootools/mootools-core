91ee860b4db2ae46f0d6634ee9117703b043542f
kamicane <kamicane@4db308c1-fb21-0410-9919-de62e267375e>
2008-03-19 16:40:49 +0000

    - fixed #900
    git-svn-id: http://svn.mootools.net/trunk@1509 4db308c1-fb21-0410-9919-de62e267375e

--- Compatibility/Core/Browser.js
+++ Compatibility/Core/Browser.js
@@ -2,7 +2,7 @@ window.extend = document.extend = function(properties){
 	for (var property in properties) this[property] = properties[property];
 };
 
-$extend(window, Browser.Engine);
+window[Browser.Engine.name] = window[Browser.Engine.name + Browser.Engine.version] = true;
 
 window.ie = window.trident;
 window.ie6 = window.trident4;
