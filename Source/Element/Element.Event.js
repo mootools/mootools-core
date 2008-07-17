888ab6a9f6eaa64865031e8f86f399e05753c724
Jan Kassens <jan@kassens.net>
2008-07-17 12:09:13 +0200

    removed unused vars

--- Source/Element/Element.Event.js
+++ Source/Element/Element.Event.js
@@ -50,7 +50,7 @@ Native.implement([Element, Window, Document], {
 		if (!events || !events[type]) return this;
 		var pos = events[type].keys.indexOf(fn);
 		if (pos == -1) return this;
-		var key = events[type].keys.splice(pos, 1)[0];
+		events[type].keys.splice(pos, 1);
 		var value = events[type].values.splice(pos, 1)[0];
 		var custom = Element.Events.get(type);
 		if (custom){
