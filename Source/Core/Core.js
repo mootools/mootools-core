59f9bb3f2ee912aeb5f4d8d72fb4d0ebc6f86b07
Tom Occhino <tomocchino@gmail.com>
2008-09-18 01:50:10 +0200

    restructured Core.js and Function.js (and specs), updated MooTools.version

--- Source/Core/Core.js
+++ Source/Core/Core.js
@@ -17,22 +17,21 @@ Inspiration:
 */
 
 var MooTools = {
-	'version': '1.2dev',
+	'version': '1.2.1dev',
 	'build': '%build%'
 };
 
 var Native = function(options){
 	options = options || {};
-
-	var afterImplement = options.afterImplement || function(){};
-	var generics = options.generics;
-	generics = (generics !== false);
-	var legacy = options.legacy;
-	var initialize = options.initialize;
-	var protect = options.protect;
 	var name = options.name;
-
+	var legacy = options.legacy;
+	var protect = options.protect;
+	var methods = options.implement;
+	var generics = options.generics;
+	var initialize = options.initialize;
+	var afterImplement = options.afterImplement || function(){};
 	var object = initialize || legacy;
+	generics = generics !== false;
 
 	object.constructor = Native;
 	object.$family = {name: 'native'};
@@ -51,28 +50,24 @@ var Native = function(options){
 		afterImplement.call(obj, name, method);
 		return obj;
 	};
+	
+	object.alias = function(a1, a2, a3){
+		if (typeof a1 == 'string'){
+			if ((a1 = this.prototype[a1])) return add(this, a2, a1, a3);
+		}
+		for (var a in a1) this.alias(a, a1[a], a2);
+		return this;
+	};
 
 	object.implement = function(a1, a2, a3){
 		if (typeof a1 == 'string') return add(this, a1, a2, a3);
 		for (var p in a1) add(this, p, a1[p], a2);
 		return this;
 	};
-
-	object.alias = function(a1, a2, a3){
-		if (typeof a1 == 'string'){
-			a1 = this.prototype[a1];
-			if (a1) add(this, a2, a1, a3);
-		} else {
-			for (var a in a1) this.alias(a, a1[a], a2);
-		}
-		return this;
-	};
-
+	
+	if (methods) object.implement(methods);
+	
 	return object;
-};
-
-Native.implement = function(objects, properties){
-	for (var i = 0, l = objects.length; i < l; i++) objects[i].implement(properties);
 };
 
 Native.genericize = function(object, property, check){
@@ -82,141 +77,31 @@ Native.genericize = function(object, property, check){
 	};
 };
 
+Native.implement = function(objects, properties){
+	for (var i = 0, l = objects.length; i < l; i++) objects[i].implement(properties);
+};
+
 Native.typize = function(object, family){
 	if (!object.type) object.type = function(item){
 		return ($type(item) === family);
 	};
 };
 
-Native.alias = function(objects, a1, a2, a3){
-	for (var i = 0, j = objects.length; i < j; i++) objects[i].alias(a1, a2, a3);
-};
+(function(){
+	var natives = {'Array': Array, 'Date': Date, 'Function': Function, 'Number': Number, 'RegExp': RegExp, 'String': String};
+	for (var n in natives) new Native({name: n, initialize: natives[n], protect: true});
 
-(function(objects){
-	for (var name in objects) Native.typize(objects[name], name);
-})({'boolean': Boolean, 'native': Native, 'object': Object});
+	var types = {'boolean': Boolean, 'native': Native, 'object': Object};
+	for (var t in types) Native.typize(types[t], t);
 
-(function(objects){
-	for (var name in objects) new Native({name: name, initialize: objects[name], protect: true});
-})({'String': String, 'Function': Function, 'Number': Number, 'Array': Array, 'RegExp': RegExp, 'Date': Date});
-
-(function(object, methods){
-	for (var i = methods.length; i--; i) Native.genericize(object, methods[i], true);
-	return arguments.callee;
-})
-(Array, ['pop', 'push', 'reverse', 'shift', 'sort', 'splice', 'unshift', 'concat', 'join', 'slice', 'toString', 'valueOf', 'indexOf', 'lastIndexOf'])
-(String, ['charAt', 'charCodeAt', 'concat', 'indexOf', 'lastIndexOf', 'match', 'replace', 'search', 'slice', 'split', 'substr', 'substring', 'toLowerCase', 'toUpperCase', 'valueOf']);
-
-function $chk(obj){
-	return !!(obj || obj === 0);
-};
-
-function $clear(timer){
-	clearTimeout(timer);
-	clearInterval(timer);
-	return null;
-};
-
-function $defined(obj){
-	return (obj != undefined);
-};
-
-function $empty(){};
-
-function $arguments(i){
-	return function(){
-		return arguments[i];
+	var generics = {
+		'Array': ["concat", "indexOf", "join", "lastIndexOf", "pop", "push", "reverse", "shift", "slice", "sort", "splice", "toString", "unshift", "valueOf"],
+		'String': ["charAt", "charCodeAt", "concat", "indexOf", "lastIndexOf", "match", "replace", "search", "slice", "split", "substr", "substring", "toLowerCase", "toUpperCase", "valueOf"]
 	};
-};
-
-function $lambda(value){
-	return (typeof value == 'function') ? value : function(){
-		return value;
+	for (var g in generics){
+		for (var i = generics[g].length; i--;) Native.genericize(window[g], generics[g][i], true);
 	};
-};
-
-function $extend(original, extended){
-	for (var key in (extended || {})) original[key] = extended[key];
-	return original;
-};
-
-function $unlink(object){
-	var unlinked;
-
-	switch ($type(object)){
-		case 'object':
-			unlinked = {};
-			for (var p in object) unlinked[p] = $unlink(object[p]);
-		break;
-		case 'hash':
-			unlinked = new Hash(object);
-		break;
-		case 'array':
-			unlinked = [];
-			for (var i = 0, l = object.length; i < l; i++) unlinked[i] = $unlink(object[i]);
-		break;
-		default: return object;
-	}
-
-	return unlinked;
-};
-
-function $merge(){
-	var mix = {};
-	for (var i = 0, l = arguments.length; i < l; i++){
-		var object = arguments[i];
-		if ($type(object) != 'object') continue;
-		for (var key in object){
-			var op = object[key], mp = mix[key];
-			mix[key] = (mp && $type(op) == 'object' && $type(mp) == 'object') ? $merge(mp, op) : $unlink(op);
-		}
-	}
-	return mix;
-};
-
-function $pick(){
-	for (var i = 0, l = arguments.length; i < l; i++){
-		if (arguments[i] != undefined) return arguments[i];
-	}
-	return null;
-};
-
-function $random(min, max){
-	return Math.floor(Math.random() * (max - min + 1) + min);
-};
-
-function $splat(obj){
-	var type = $type(obj);
-	return (type) ? ((type != 'array' && type != 'arguments') ? [obj] : obj) : [];
-};
-
-var $time = Date.now || function(){
-	return new Date().getTime();
-};
-
-function $try(){
-	for (var i = 0, l = arguments.length; i < l; i++){
-		try {
-			return arguments[i]();
-		} catch(e){}
-	}
-	return null;
-};
-
-function $type(obj){
-	if (obj == undefined) return false;
-	if (obj.$family) return (obj.$family.name == 'number' && !isFinite(obj)) ? false : obj.$family.name;
-	if (obj.nodeName){
-		switch (obj.nodeType){
-			case 1: return 'element';
-			case 3: return (/\S/).test(obj.nodeValue) ? 'textnode' : 'whitespace';
-		}
-	} else if (typeof obj.length == 'number'){
-		if (obj.callee) return 'arguments';
-		else if (obj.item) return 'collection';
-	}
-	return typeof obj;
-};
+})();
 
 var Hash = new Native({
 
@@ -253,14 +138,10 @@ Hash.implement({
 		}
 		return clean;
 	}
-
+	
 });
 
 Hash.alias('forEach', 'each');
-
-function $H(object){
-	return new Hash(object);
-};
 
 Array.implement({
 
@@ -281,7 +162,120 @@ function $A(iterable){
 	return Array.prototype.slice.call(iterable);
 };
 
+function $arguments(i){
+	return function(){
+		return arguments[i];
+	};
+};
+
+function $chk(obj){
+	return !!(obj || obj === 0);
+};
+
+function $clear(timer){
+	clearTimeout(timer);
+	clearInterval(timer);
+	return null;
+};
+
+function $defined(obj){
+	return (obj != undefined);
+};
+
 function $each(iterable, fn, bind){
 	var type = $type(iterable);
 	((type == 'arguments' || type == 'collection' || type == 'array') ? Array : Hash).each(iterable, fn, bind);
+};
+
+function $empty(){};
+
+function $extend(original, extended){
+	for (var key in (extended || {})) original[key] = extended[key];
+	return original;
+};
+
+function $H(object){
+	return new Hash(object);
+};
+
+function $lambda(value){
+	return (typeof value == 'function') ? value : function(){
+		return value;
+	};
+};
+
+function $merge(){
+	var mix = {};
+	for (var i = 0, l = arguments.length; i < l; i++){
+		var object = arguments[i];
+		if ($type(object) != 'object') continue;
+		for (var key in object){
+			var op = object[key], mp = mix[key];
+			mix[key] = (mp && $type(op) == 'object' && $type(mp) == 'object') ? $merge(mp, op) : $unlink(op);
+		}
+	}
+	return mix;
+};
+
+function $pick(){
+	for (var i = 0, l = arguments.length; i < l; i++){
+		if (arguments[i] != undefined) return arguments[i];
+	}
+	return null;
+};
+
+function $random(min, max){
+	return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+function $splat(obj){
+	var type = $type(obj);
+	return (type) ? ((type != 'array' && type != 'arguments') ? [obj] : obj) : [];
+};
+
+var $time = Date.now || function(){
+	return +new Date;
+};
+
+function $try(){
+	for (var i = 0, l = arguments.length; i < l; i++){
+		try {
+			return arguments[i]();
+		} catch(e){}
+	}
+	return null;
+};
+
+function $type(obj){
+	if (obj == undefined) return false;
+	if (obj.$family) return (obj.$family.name == 'number' && !isFinite(obj)) ? false : obj.$family.name;
+	if (obj.nodeName){
+		switch (obj.nodeType){
+			case 1: return 'element';
+			case 3: return (/\S/).test(obj.nodeValue) ? 'textnode' : 'whitespace';
+		}
+	} else if (typeof obj.length == 'number'){
+		if (obj.callee) return 'arguments';
+		else if (obj.item) return 'collection';
+	}
+	return typeof obj;
+};
+
+function $unlink(object){
+	var unlinked;
+	switch ($type(object)){
+		case 'object':
+			unlinked = {};
+			for (var p in object) unlinked[p] = $unlink(object[p]);
+		break;
+		case 'hash':
+			unlinked = new Hash(object);
+		break;
+		case 'array':
+			unlinked = [];
+			for (var i = 0, l = object.length; i < l; i++) unlinked[i] = $unlink(object[i]);
+		break;
+		default: return object;
+	}
+	return unlinked;
 };
