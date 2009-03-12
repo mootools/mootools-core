d478d08351604735be0bfad7863947f09455c42b
Valerio Proietti <kamicane@gmail.com>
2009-03-12 22:45:12 +0100

    - The "fix" part of Browser.js has been cleaned up a little. The objects we overwrite in MooTools (Element, Document, Window, Event) are now saved in Browser for future accesses.

--- Source/Browser/Browser.js
+++ Source/Browser/Browser.js
@@ -6,9 +6,7 @@ License:
 	MIT-style license.
 */
 
-var Browser = function(){};
-
-Browser.extend({
+var Browser = (function(){}).extend({
 
 	Engine: {name: 'unknown', version: 0},
 
@@ -42,22 +40,20 @@ Browser.extend({
 
 Browser.Platform[Browser.Platform.name] = true;
 
-Browser.detect = function(){
+(function(){
 
-	for (var engine in this.Engines){
-		var version = this.Engines[engine]();
+	for (var engine in Browser.Engines){
+		var version = Browser.Engines[engine]();
 		if (version){
-			this.Engine = {name: engine, version: version};
-			this.Engine[engine] = this.Engine[engine + version] = true;
+			Browser.Engine = {name: engine, version: version};
+			Browser.Engine[engine] = Browser.Engine[engine + version] = true;
 			break;
 		}
 	}
 
-	return {name: engine, version: version};
+})();
 
-};
-
-Browser.detect();
+// Request
 
 Browser.Request = function(){
 	return Function.stab(function(){
@@ -69,102 +65,96 @@ Browser.Request = function(){
 
 Browser.Features.xhr = !!(Browser.Request());
 
+// Flash detection
+
 Browser.Plugins.Flash = (function(){
 	var version = (Function.stab(function(){
 		return navigator.plugins['Shockwave Flash'].description;
 	}, function(){
 		return new ActiveXObject('ShockwaveFlash.ShockwaveFlash').GetVariable('$version');
 	}) || '0 r0').match(/\d+/g);
-	return {version: Number.from(version[0] || 0 + '.' + version[1]) || 0, build: Number.from(version[2]) || 0};
+	return {version: Number(version[0] || 0 + '.' + version[1]) || 0, build: Number(version[2]) || 0};
 })();
 
-Native.UID = 1;
+// String scripts
 
-Native.uid = (Browser.Engine.trident) ? function(item){
-	return (item.uid || (item.uid = [Native.UID++]))[0];
-} : function(item){
-	return item.uid || (item.uid = Native.UID++);
-};
-
-String.extend({
+String.extend('exec', function(text){
 	
-	exec: function(text){
-		if (!text) return text;
-		if (window.execScript){
-			window.execScript(text);
-		} else {
-			var script = document.createElement('script');
-			script.setAttribute('type', 'text/javascript');
-			script[(Browser.Engine.webkit && Browser.Engine.version < 420) ? 'innerText' : 'text'] = text;
-			document.head.appendChild(script);
-			document.head.removeChild(script);
-		}
-		return text;
+	if (!text) return text;
+	if (window.execScript){
+		window.execScript(text);
+	} else {
+		var script = document.createElement('script');
+		script.setAttribute('type', 'text/javascript');
+		script[(Browser.Engine.webkit && Browser.Engine.version < 420) ? 'innerText' : 'text'] = text;
+		document.head.appendChild(script);
+		document.head.removeChild(script);
 	}
+	return text;
 	
-});
-
-String.implement({
+}).implement('stripScripts', function(option){
 	
-	stripScripts: function(option){
-		var scripts = '';
-		var text = this.replace(/<script[^>]*>([\s\S]*?)<\/script>/gi, function(){
-			scripts += arguments[1] + '\n';
-			return '';
-		});
-		if (option === true) String.exec(scripts);
-		else if (typeOf(option) == 'function') option(scripts, text);
-		return text;
-	}
+	var scripts = '';
+	var text = this.replace(/<script[^>]*>([\s\S]*?)<\/script>/gi, function(){
+		scripts += arguments[1] + '\n';
+		return '';
+	});
+	if (option === true) String.exec(scripts);
+	else if (typeOf(option) == 'function') option(scripts, text);
+	return text;
 
 });
 
-(function(){
+// Window, Document
 
-var legacyWindow = window.Window;
+(function(win, doc){
+	
+	if (!win.Window) win.Window = (function Window(){});
+	Browser.Window = win.Window;
+	
+	win.Window = new Native(function Window(){});
 
-window.Window = new Native('Window', function(win){
 	if (!win.Element){
-		win.Element = function(){};
-		if (Browser.Engine.webkit) win.document.createElement("iframe"); //fixes safari 2
-		win.Element.prototype = (Browser.Engine.webkit) ? window["[[DOMElement.prototype]]"] : {};
+		var Element = (function Element(){});
+		if (Browser.Engine.webkit) doc.createElement("iframe"); //fixes safari 2
+		Element.prototype = (Browser.Engine.webkit) ? win["[[DOMElement.prototype]]"] : {};
 	}
-	win.document.window = win;
-	if (!legacyWindow){
-		win.constructor = Window;
-		win._type = Function.from('window');
-	}
-}, legacyWindow);
+	Browser.Element = win.Element;
 
-if (!legacyWindow) Window._beforeImplement = function(name, method){
-	return (window[name] = method);
-};
+	if (!win.Event) win.Event = (function Event(){});
+	Browser.Event = win.Event;
 
-var legacyDocument = window.Document;
+	doc.window = win;
 
-window.Document = new Native('Document', function(doc){
+	win.constructor = Window;
+	win[':type'] = Function.from('window').hide();
+
+	Window.mirror(function(name, method){
+		win[name] = method;
+	}).implement(new Storage);
+
+	if (!win.Document) win.Document = (function Document(){});
+	Browser.Document = win.Document;
+	
+	win.Document = new Native(function Document(){});
+
 	doc.head = doc.getElementsByTagName('head')[0];
 	doc.html = doc.getElementsByTagName('html')[0];
 	if (Browser.Engine.trident){
 		if (Browser.Engine.version <= 4) Function.stab(function(){
 			doc.execCommand("BackgroundImageCache", false, true);
 		});
-		doc.window.attachEvent('onunload', function(){
-			doc.window.detachEvent('onunload', arguments.callee);
+		win.attachEvent('onunload', function(){
+			win.detachEvent('onunload', arguments.callee);
 			doc.head = doc.html = doc.window = null;
 		});
 	}
-	if (!legacyDocument){
-		doc.constructor = Document;
-		doc._type = Function.from('document');
-	}
-}, legacyDocument);
 
-if (!legacyDocument) Document._beforeImplement = function(name, method){
-	return (document[name] = method);
-};
+	doc.constructor = Document;
+	doc[':type'] = Function.from('document').hide();
 
-})();
-
-new Window(window);
-new Document(document);
+	Document.mirror(function(name, method){
+		doc[name] = method;
+	}).implement(new Storage);
+	
+})(window, document);
