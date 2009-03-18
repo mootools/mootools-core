785c16bab18871918d3de8d6c04bc1f2c6b13faa
Christoph Pojer <christoph.pojer@gmail.com>
2009-03-18 14:13:04 +0100

    * Minor Array cleanup

--- Source/Native/Array.js
+++ Source/Native/Array.js
@@ -17,8 +17,7 @@ Array.implement({
 	},
 
 	indexOf: function(item, from){
-		var len = this.length;
-		for (var i = (from < 0) ? Math.max(0, len + from) : from || 0; i < len; i++){
+		for (var l = this.length, i = (from < 0) ? Math.max(0, l + from) : from || 0; i < l; i++){
 			if (this[i] === item) return i;
 		}
 		return -1;
