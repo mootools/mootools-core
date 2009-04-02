2e7535eb567ca950887331a9d0da8e02a4d4a71e
Aaron Newton <git@clientcide.com>
2009-04-01 21:00:25 -0700

    adding element offsets test; removing errant test files from last commit

---
+++ Tests/UserTests/Element/Element.Dimensions.offsets.js
@@ -0,0 +1,10 @@
+{
+	tests: [
+		{
+			title: "Element.offsets",
+			description: "Tests the offset logic for elements.",
+			verify: "When you scroll does offset value remain fixed in place?"
+		}
+	],
+	otherScripts: ['DomReady']
+}
