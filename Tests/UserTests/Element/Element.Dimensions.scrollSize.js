2a28f6c63565e330b62fa582e0bae555b04770af
Aaron Newton <git@clientcide.com>
2009-04-14 14:39:08 -0700

    adding tests for offset calculation

---
+++ Tests/UserTests/Element/Element.Dimensions.scrollSize.js
@@ -0,0 +1,16 @@
+{
+	tests: [
+		{
+			title: "Element.scrollSize",
+			description: "Checks the scrollsize of the document.",
+			verify: "Do the sizes injected into the document content seem correct (the scrollsize should be larger than the frame size)?",
+			before: function(){
+				$('value').set('html', 'frame size: {frame}, scroll size: {scroll}'.substitute({
+					frame: document.body.getSize().y,
+					scroll: document.body.getScrollSize().y
+				}));
+			}
+		}
+	],
+	otherScripts: ['DomReady']
+}
