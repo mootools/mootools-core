3c17e816fc5ea8d0d5816cf4a8215c9e397797bd
Fábio M. Costa <fabio@meiocodigo.com>
2009-05-13 22:11:35 -0300

    Now specs works with Opera, i don't understand this crazy bug that is happening...

--- Specs/Assets/Scripts/Builder.js
+++ Specs/Assets/Scripts/Builder.js
@@ -10,29 +10,30 @@ Note:
 */
 
 var Builder = {
-
 	includeFiles: function(path, files){
-		if( typeof files == 'object' && files.constructor != Array ){
-			for(key in files){
-				this.includeFiles(path + key + '/', files[key]);
+		switch(typeof files){
+		case 'object':
+			if( files.constructor == Array ){
+				for(var i=0; i< files.length; i++){
+					this.includeJs(path + files[i]);
+				}
 			}
-		}
-		else if( typeof files == 'object' && files.constructor == Array ){
-			for(var i=0; i< files.length; i++){
-				this.includeJs(path + files[i]);
+			else{
+				for(key in files){
+					this.includeFiles(path + key + '/', files[key]);
+				}
 			}
-		}
-		else if( typeof files == 'string'){
+		break;
+		case 'string':
 			this.includeJs(path + files);
 		}
 	},
 	
 	includeJs: function(filePath){
-		document.writeln('<script type="text/javascript" src="' + filePath + '.js?' + new Date().getTime() + '"></script>');
+		document.writeln('<script type="text/javascript" src="' + filePath + '.js?' + new Date().getTime() + '"><\/script>');
 	},
 	
 	build: function(root, filesTree){
 		this.includeFiles(root, filesTree);
 	}
-
 };
