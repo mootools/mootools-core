5ec444180e13cbd4dc50827963cfd55f4bacb924
Ian Collins <ian.collins@gmail.com>
2009-05-15 14:19:51 -0700

    fix for webkit body.scrollTop inconsistency

--- Source/Element/Element.Dimensions.js
+++ Source/Element/Element.Dimensions.js
@@ -61,11 +61,13 @@ Element.implement({
 
 	getOffsets: function(){		
 		if (this.getBoundingClientRect){			
-			var bound = this.getBoundingClientRect(), html = this.getDocument().documentElement;
-			var isFixed = styleString(this, 'position') == 'fixed';
+			var bound   = this.getBoundingClientRect(), 
+					html    = this.getDocument().documentElement
+					scroll  = html.getScroll();
+					isFixed = styleString(this, 'position') == 'fixed';
 			return {
-				x: parseInt(bound.left, 10) + ((isFixed) ? 0 : html.scrollLeft) - html.clientLeft,
-				y: parseInt(bound.top, 10) +  ((isFixed) ? 0 : html.scrollTop)  - html.clientTop
+				x: parseInt(bound.left, 10) + ((isFixed) ? 0 : scroll.x) - html.clientLeft,
+				y: parseInt(bound.top, 10) +  ((isFixed) ? 0 : scroll.y) - html.clientTop
 			};
 		}
 
