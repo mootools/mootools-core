08bb6167c6c8bbdab091880b42cc93f314b41b60
Thomas Aylott <oblivious+git@subtlegradient.com>
2009-06-18 16:39:35 -0400

    Revert removing doScroll
    This reverts commit 2dbd3a1eb2be0c60e2fa8c61cec0697f3404deea.

--- Source/Utilities/DomReady.js
+++ Source/Utilities/DomReady.js
@@ -27,6 +27,7 @@ Element.Events.domready = {
 		var temp = document.createElement('div');
 		(function(){
 			($try(function(){
+				temp.doScroll('left');
 				return $(temp).inject(document.body).set('html', 'temp').dispose();
 			})) ? domready() : arguments.callee.delay(50);
 		})();
