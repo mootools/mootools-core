c6f3e0b6db93012b8003d0c0bbf4c1e64d18ee19
Valerio Proietti <kamicane@gmail.com>
2009-07-02 18:46:58 +0200

    - moved Accessor.js to the Utilities folder.  - accessor .js now creates singular and plural lookups / defines functions.  - if Accessor is called without a context (example: Accessor(name)) it will return a function to create the accessor with that name (useful for implementations).  - Accessor also creates a matcher. When you define something as a regular expression, you can retrieve it with match. matches will be passed as additional arguments (it's actually a big deal :)).

---
+++ Source/Utilities/Accessor.js
@@ -0,0 +1,61 @@
+/*=
+name: Accessor
+description: yo!
+requires:
+  - Array
+  - Function
+  - Number
+  - String
+  - Object
+  - Table
+=*/
+
+(function(global){
+
+/* Accessor */
+
+this.Accessor = function(singular, plural){
+	
+	if (this === global) return function(){
+		return new Accessor(singular, plural);
+	};
+	
+	singular = (singular || '').capitalize();
+	if (!plural) plural = singular + 's';
+	
+	var accessor = {}, matchers = [];
+	
+	this['define' + singular] = function(key, value){
+		if (typeOf(key) == 'regexp') matchers.push({'regexp': key, 'action': value});
+		else accessor[key] = value;
+		return this;
+	};
+	
+	this['define' + plural] = function(object){
+		for (var key in object) accessor[key] = object[key];
+		return this;
+	};
+	
+	this['match' + singular] = function(name){
+		for (var l = matchers.length; l--; l){
+			var matcher = matchers[l], match = name.match(matcher.regexp);
+			if (match && (match = match.slice(1))) return function(){
+				return matcher.action.apply(this, Array.slice(arguments).append(match));
+			};
+		}
+		return null;
+	};
+	
+	this['lookup' + singular] = function(key){   
+		return accessor[key] || null;
+	};
+	
+	this['lookup' + plural] = function(keys){
+		return Object.subset(accessor, keys);
+	};
+	
+	return this;
+
+};
+
+})(this);
