d7fbfc3f25172dea8c23d8a433715c7ac84835e3
Scott Kyle <scott@appden.com>
2009-08-13 01:25:01 -0400

    User Agent (UA) detection in Browser. API subject to change. Version detected only with one decimal, so 3.5.2 would evaluate to 3.5.  This is to make numerical comparisons possible.

--- Source/Browser/Browser.js
+++ Source/Browser/Browser.js
@@ -7,52 +7,34 @@ requires:
 
 (function(){
 
+var document = this.document;
+var window = document.window = this;
+
+var UA = navigator.userAgent.toLowerCase().match(/(opera|msie|firefox|chrome|safari)[\s\/:](\d+(?:\.\d+)?)?/) || [null, 'unknown', 0];
+
 var Browser = this.Browser = (function(){}).extend({
+	
+	name: UA[1],
+	version: parseFloat(UA[2]),
 
-	Engine: {name: 'unknown', version: 0},
-
-	Platform: {name: (window.orientation != null) ? 'ipod' : (navigator.platform.match(/mac|win|linux/i) || ['other'])[0].toLowerCase()},
+	Platform: {
+		name: (this.orientation != null) ? 'ipod' : (navigator.platform.toLowerCase().match(/mac|win|linux/) || ['other'])[0]
+	},
 
 	Features: {
 		xpath: !!(document.evaluate),
 		air: !!(window.runtime),
-		query: !!(document.querySelector)
+		query: !!(document.querySelector),
+		json: !!(window.JSON)
 	},
 
 	Plugins: {}
 
 });
 
+Browser[Browser.name] = true;
+Browser[Browser.name + parseInt(Browser.version, 10)] = true;
 Browser.Platform[Browser.Platform.name] = true;
-
-var engines = {
-
-	presto: function(){
-		return (!window.opera) ? false : ((arguments.callee.caller) ? 960 : ((document.getElementsByClassName) ? 950 : 925));
-	},
-
-	trident: function(){
-		return (!window.ActiveXObject) ? false : ((Browser.Features.json) ? 6 : ((window.XMLHttpRequest) ? 5 : 4));
-	},
-
-	webkit: function(){
-		return (navigator.taintEnabled) ? false : ((Browser.Features.xpath) ? ((Browser.Features.query) ? 525 : 420) : 419);
-	},
-
-	gecko: function(){
-		return (document.getBoxObjectFor == null) ? false : ((document.getElementsByClassName) ? ((Browser.Features.query) ? 19.1 : 19) : 18);
-	}
-
-};
-
-for (var engine in engines){
-	var version = engines[engine]();
-	if (version){
-		Browser.Engine = {name: engine, version: version};
-		Browser.Engine[engine] = Browser.Engine[engine + version] = true;
-		break;
-	}
-}
 
 // Request
 
@@ -143,29 +125,25 @@ Window.mirror(function(name, method){
 
 Object.append(window, new Storage);
 
-var doc = this.document;
-doc.window = this;
+document.head = document.getElementsByTagName('head')[0];
+document.html = document.getElementsByTagName('html')[0];
 
-doc.head = doc.getElementsByTagName('head')[0];
-doc.html = doc.getElementsByTagName('html')[0];
-
-
-if (doc.execCommand) Function.stab(function(){
-	doc.execCommand("BackgroundImageCache", false, true);
+if (document.execCommand) Function.stab(function(){
+	document.execCommand("BackgroundImageCache", false, true);
 });
 
 if (this.attachEvent) this.attachEvent('onunload', function(){
 	this.detachEvent('onunload', arguments.callee);
-	doc.head = doc.html = doc.window = null;
+	document.head = document.html = document.window = null;
 });
 
 this.Document = new Type('Document', function(){});
 
-doc.constructor = this.Document;
-doc.$typeOf = Function.from('document').hide();
+document.constructor = this.Document;
+document.$typeOf = Function.from('document').hide();
 
 Document.mirror(function(name, method){
-	doc[name] = method;
+	document[name] = method;
 });
 
 Object.append(document, new Storage);
