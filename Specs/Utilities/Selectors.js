bdd69a036fc0764bd2b31141df1e1c7264817415
Fabio M. Costa <fabio@macfabio.(none)>
2009-10-06 21:11:24 -0300

    Spec showing, that the star selector (*) selects comment nodes.

--- Specs/Utilities/Selectors.js
+++ Specs/Utilities/Selectors.js
@@ -6,3 +6,12 @@ License:
 	MIT-style license.
 */
 
+describe('Utilities.Selectors', {
+
+	'should not return comment nodes': function(){
+		var div = new Element('div');
+		div.appendChild(document.createComment(''));
+		value_of(div.getElements('*').length).should_be(0);
+	}
+
+});
