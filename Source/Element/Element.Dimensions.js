f627a28c6b30cae914412d6478ebc10c3b3761f0
Scott Kyle <scott@appden.com>
2009-10-11 14:19:56 -0700

    Change getOffsets to use toInt rather than parseInt

--- Source/Element/Element.Dimensions.js
+++ Source/Element/Element.Dimensions.js
@@ -69,8 +69,8 @@ Element.implement({
 				isFixed = (styleString(this, 'position') == 'fixed');
 
 			return {
-				x: parseInt(bound.left, 10) + elemScrolls.x - elemScroll.x + ((isFixed) ? 0 : htmlScroll.x) - html.clientLeft,
-				y: parseInt(bound.top, 10)  + elemScrolls.y - elemScroll.y + ((isFixed) ? 0 : htmlScroll.y) - html.clientTop
+				x: bound.left.toInt() + elemScrolls.x - elemScroll.x + ((isFixed) ? 0 : htmlScroll.x) - html.clientLeft,
+				y: bound.top.toInt()  + elemScrolls.y - elemScroll.y + ((isFixed) ? 0 : htmlScroll.y) - html.clientTop
 			};
 		}
 
