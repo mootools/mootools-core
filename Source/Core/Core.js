7d16ac0e7cea09c4d2cd15a1fc7e18410418f508
Aaron Newton <git@clientcide.com>
2009-10-19 15:00:06 -0700

    welcome to 1.2.5dev

--- Source/Core/Core.js
+++ Source/Core/Core.js
@@ -21,8 +21,8 @@ provides: [Mootools, Native, Hash.base, Array.each, $util]
 */
 
 var MooTools = {
-	'version': '1.2.4',
-	'build': '0d9113241a90b9cd5643b926795852a2026710d4'
+	'version': '1.2.5dev',
+	'build': '%build%'
 };
 
 var Native = function(options){
