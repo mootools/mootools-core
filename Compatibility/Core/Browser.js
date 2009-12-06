a3c6fc35ccfcec8693881ec590f9cca25d413dc5
Christoph Pojer <christoph.pojer@gmail.com>
2009-12-06 02:45:08 +0100

    * Removing old Compatibility Layer

--- Compatibility/Core/Browser.js
+++
@@ -1,9 +0,0 @@
-window.extend = document.extend = function(properties){
-	for (var property in properties) this[property] = properties[property];
-};
-
-window[Browser.Engine.name] = window[Browser.Engine.name + Browser.Engine.version] = true;
-
-window.ie = window.trident;
-window.ie6 = window.trident4;
-window.ie7 = window.trident5;
--- Compatibility/Native/Function.js
+++
@@ -1,9 +0,0 @@
-Function.extend({
-
-	bindAsEventListener: function(bind, args){
-		return this.create({'bind': bind, 'event': true, 'arguments': args});
-	}
-
-});
-
-Function.empty = $empty;
