fe807a5df2463ae5e15703875906451196ac2e83
Christoph Pojer <christoph.pojer@gmail.com>
2009-12-06 03:58:34 +0100

    * Removing the dependency of Array.prototype.forEach (for IE)

--- SlickSpec/Loader.js
+++ SlickSpec/Loader.js
@@ -1,15 +1,21 @@
 var loadSpecs = (function(Sets){
 
+var forEach = function(array, fn, bind){
+	for (var i = 0, l = array.length; i < l; i++){
+		if (i in array) fn.call(bind, array[i], i, array);
+	}
+};
+
 // Uses String.parseQueryString from MooTools-More
 // TODO Needs compat for other browsers
 var parseQueryString = function(string){
 	var vars = string.split(/[&;]/), res = {};
-	if (vars.length) vars.forEach(function(val){
+	if (vars.length) forEach(vars, function(val){
 		var index = val.indexOf('='),
 			keys = index < 0 ? [''] : val.substr(0, index).match(/[^\]\[]+/g),
 			value = decodeURIComponent(val.substr(index + 1)),
 			obj = res;
-		keys.forEach(function(key, i){
+		forEach(keys, function(key, i){
 			var current = obj[key];
 			if(i < keys.length - 1)
 				obj = obj[key] = current || {};
@@ -28,7 +34,7 @@ var getSpecs = function(queryString){
 	var requestedSpecs = [],
 		specs = queryString.specs;
 	
-	(specs && specs._type == 'Array' ? specs : [specs]).forEach(function(spec){
+	forEach(specs && specs._type == 'Array' ? specs : [specs], function(spec){
 		if (Sets[spec] && requestedSpecs.indexOf(spec) == -1) requestedSpecs.push(spec);
 	});
 	
