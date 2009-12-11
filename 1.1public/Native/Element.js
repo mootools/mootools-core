fd63fb9b71564087dd574b841f9f0cd4c37ba17c
Aaron Newton <git@clientcide.com>
2009-12-10 16:21:31 -0800

    adding element style specs.

--- 1.1public/Native/Element.js
+++ 1.1public/Native/Element.js
@@ -317,3 +317,69 @@ describe('injection', {
 	
 });
 })();
+
+describe('Element.set `opacity`', {
+
+	'should set the opacity of an Element': function() {
+		var el = new Element('div').setStyle('opacity', 0.4);
+		if (window.ie) value_of(el.style.filter).should_be('alpha(opacity=40)');
+		value_of(el.style.opacity).should_be(0.4);
+	},
+
+	'should return the opacity of an Element': function() {
+		value_of(new Element('div').setStyle('opacity', 0.4).getStyle('opacity')).should_be(0.4);
+	}
+
+});
+
+describe('Element.getStyle', {
+
+	'should get a six digit hex code from a three digit hex code': function() {
+		var el = new Element('div').setHTML('<div style="color:#00ff00"></div>');
+		value_of(el.getElement('div').getStyle('color')).should_be('#00ff00');
+	},
+
+	'should getStyle a six digit hex code from an RGB value': function() {
+		var el = new Element('div').setHTML('<div style="color:rgb(0, 255, 0)"></div>');
+		value_of(el.getElement('div').getStyle('color')).should_be('#00ff00');
+	},
+
+	'should `getStyle` with a dash in it': function() {
+		var el = new Element('div').setHTML('<div style="list-style-type:square"></div>');
+		value_of(el.getElement('div').getStyle('list-style-type')).should_be('square');
+	}
+
+});
+
+describe('Element.setStyle', {
+
+	'should set the `styles` property on an Element using the Element constructor': function() {
+		value_of(new Element('div', {styles:{'color':'#00ff00'}}).getStyle('color')).should_be('#00ff00');
+	},
+
+	'should `setStyle` on an Element': function() {
+		value_of(new Element('div').setStyle('color','#00ff00').getStyle('color')).should_be('#00ff00');
+	},
+
+	'should properly `setStyle` for a property with a dash in it': function() {
+		value_of(new Element('div').setStyle('list-style-type', 'square').getStyle('list-style-type')).should_be('square');
+	}
+
+});
+
+describe('Element.getStyles', {
+
+	'should return multiple styles': function() {
+		var el = new Element('div').setHTML('<div style="color:#00ff00;list-style-type:square"></div>');
+		value_of(el.getElement('div').getStyles('color', 'list-style-type')).should_be({color:'#00ff00', 'list-style-type':'square'});
+	}
+
+});
+
+describe('Element.setStyles', {
+
+	'should set multiple styles': function() {
+		value_of(new Element('div').setStyles({'list-style-type':'square', 'color':'#00ff00'}).getStyles('list-style-type', 'color')).should_be({'list-style-type':'square', color:'#00ff00'});
+	}
+
+});
