514d39965f377550349426c84c8215adcc10098f
Christoph Pojer <christoph.pojer@gmail.com>
2009-12-16 15:51:23 +0100

    * Sets change

--- Sets.js
+++ Sets.js
@@ -1,9 +1,15 @@
-var Sets = {
-
-	'1.1public': ['Core/Core.js', 'Native/Array.js', 'Native/Function.js', 'Native/Number.js', 'Native/String.js', 'Native/Element.js', 'Element/Element.Dimensions.js', 'Element/Element.Form.js', 'Element/Element.Filters.js', 'Element/Element.Selectors.js', 'Element/Element.Event.js', 'Class/Class.js', 'Class/Class.Extras.js', 'Remote/Ajax.js', 'Remote/Cookie.js', 'Remote/Json.js', 'Remote/XHR.js'],
+(function(context){
 	
-	'1.1private': [],
+context.Sets = {
 
+	'1.1public': [
+		'Core/Core.js',
+		'Native/Array.js', 'Native/Function.js', 'Native/Number.js', 'Native/String.js', 'Native/Element.js',
+		'Class/Class.js', 'Class/Class.Extras.js',
+		'Element/Element.Dimensions.js', 'Element/Element.Form.js', 'Element/Element.Filters.js',
+		'Element/Element.Selectors.js', 'Element/Element.Event.js',
+		'Remote/Ajax.js', 'Remote/Cookie.js', 'Remote/Json.js', 'Remote/XHR.js'],
+	
 	'1.1compat': [
 		'Core/Browser.js', 'Class/Class.js',
 		'Native/Array.js', 'Native/Function.js', 'Native/Hash.js',
@@ -29,3 +35,5 @@ var Sets = {
 	]
 
 };
+
+})(this.exports || this);
