0e88d367bb63d1167e159ae499ef1852968835be
Darren Waddell <fakedarren@Jeffrey.local>
2009-12-25 15:38:38 +0000

    Added 'properties' setter

--- Compatibility/Element/Element.js
+++ Compatibility/Element/Element.js
@@ -1,128 +1,152 @@
 
 Element.extend = function(obj){
-	console.warn('Element.extend is deprecated. Use Element.implement');
+	console.warn('1.1 > 1.2: Element.extend is deprecated. Use Element.implement');
 	Element.implement(obj);
 };
 
 Elements.extend = function(obj){
-	console.warn('Elements.extend is deprecated. Use Elements.implement');
+	console.warn('1.1 > 1.2: Elements.extend is deprecated. Use Elements.implement');
 	Elements.implement(obj);
 };
 
 Element.implement({
 	
 	remove : function(){
-		console.warn('el.remove is deprecated. Use el.dispose');
+		console.warn('1.1 > 1.2: Element.remove is deprecated - use Element.dispose.'); 
 		return this.dispose();
 	},
 	
 	getLastChild : function(){
-		console.warn('el.getLastChild is deprecated. Use el.getLast');
+		console.warn('1.1 > 1.2: Element.getLastChild is deprecated. Use Element.getLast');
 		return this.getLast();
 	},
 
-    getFormElements: function(){
-		console.warn('el.getFormElements is deprecated.');
-        return this.getElements('input, textarea, select');
-    },
+	getFormElements: function(){
+		console.warn('1.1 > 1.2: Element.getFormElements is deprecated.');
+		return this.getElements('input, textarea, select');
+	},
 
-    replaceWith: function(el){
-		console.warn('el.replaceWith is deprecated.');
-        el = $(el);
-        $(this.parentNode).replaceChild(el, this); // wrapped for ie
-        return el;
-    },
-    
-    removeElements: function(){
-		console.warn('el.removeElements is deprecated. use el.dispose()');
-        return this.dispose();
-    },
+	replaceWith: function(el){
+		console.warn('1.1 > 1.2: Element.replaceWith is deprecated.');
+		el = $(el);
+		$(this.parentNode).replaceChild(el, this); // wrapped for ie
+		return el;
+	},
 
-    getText: function(){
-		console.warn('el.getText is deprecated. use el.get("text")');
-        return this.get('text');
-    },
+	removeElements: function(){
+		console.warn('1.1 > 1.2: Element.removeElements is deprecated. use Element.dispose()');
+				return this.dispose();
+	},
 
-    setText: function(text){
-		console.warn('el.setText is deprecated. use el.set("text",value)');
-        return this.set('text', text);
-    },
+	getText: function(){
+		console.warn('1.1 > 1.2: Element.getText is deprecated - use Element.get("text").'); 
+		return this.get('text');
+	},
 
-    setHTML: function(){
-		console.warn('el.setHTML is deprecated. use el.set("html",value)');
-        return this.set('html', arguments);
-    },
-    
-    getHTML: function(){
-		console.warn('el.getHTML is deprecated. use el.get("html")');
-        return this.get('html');
-    },
+	setText: function(text){
+		console.warn('1.1 > 1.2: Element.setText is deprecated. use Element.set("text",value)');
+		return this.set('text', text);
+	},
 
-    getTag: function(){
-		console.warn('el.getTag is deprecated. use el.get("tag")');
-        return this.get('tag');
-    },
+	setHTML: function(){
+		console.warn('1.1 > 1.2: Element.setHTML is deprecated. use Element.set("html",value)');
+		return this.set('html', arguments);
+	},
 
-    setOpacity: function(op){
-		console.warn('el.setOpacity is deprecated. use el.set("opacity",value)');
-        return this.set('opacity', op);
-    },
+	getHTML: function(){
+		console.warn('1.1 > 1.2: Element.getHTML is deprecated. use Element.get("html")');
+		return this.get('html');
+	},
 
-    getValue: function(){
-		console.warn('el.getValue is deprecated. use el.get("value")');
-        return this.get('value')
-    },
-    
-    // Very slightly modified from mootools
-    toQueryString: function(){
-		console.warn('warning el.toQueryString is slightly different');
-        var queryString = [];
-        this.getElements('input, select, textarea').each(function(el){
-            if (!el.name || el.disabled) return;
-            var value = (el.tagName.toLowerCase() == 'select') ? Element.getSelected(el).map(function(opt){
-                return opt.value;
-            }) : ((el.type == 'radio' || el.type == 'checkbox') && !el.checked) ? null : el.value;
-            $splat(value).each(function(val){
-                /*if (val)*/ queryString.push(el.name + '=' + encodeURIComponent(val));
-            });
-        });
-        return queryString.join('&');
-    },
+	getTag: function(){
+		console.warn('1.1 > 1.2: Element.getTag is deprecated. use Element.get("tag")');
+		return this.get('tag');
+	},
 
-    effect: function(property, options){
-		console.warn('el.effect is deprecated. use el.tween');
-        return new Fx.Tween(this, $extend({property: property}, options));
-    },
+	setOpacity: function(op){
+		console.warn('1.1 > 1.2: Element.setOpacity is deprecated. use Element.set("opacity",value)');
+		return this.set('opacity', op);
+	},
 
-    effects: function(options){
-		console.warn('el.effects is deprecated. use el.morph');
-        return new Fx.Morph(this, options);
-    },
+	getValue: function(){
+		console.warn('1.1 > 1.2: Element.getValue is deprecated. use Element.get("value")');
+		return this.get('value');
+	},
+		
+	// Very slightly modified from mootools
+	toQueryString: function(){
+		console.warn('warning Element.toQueryString is slightly different');
+		var queryString = [];
+		this.getElements('input, select, textarea').each(function(el){
+				if (!Element.name || Element.disabled) return;
+				var value = (el.tagName.toLowerCase() == 'select') ? Element.getSelected(el).map(function(opt){
+						return opt.value;
+				}) : ((el.type == 'radio' || el.type == 'checkbox') && !el.checked) ? null : el.value;
+				$splat(value).each(function(val){
+						/*if (val)*/ queryString.push(el.name + '=' + encodeURIComponent(val));
+				});
+		});
+		return queryString.join('&');
+	},
 
-    filterByTag: function(tag){
-		console.warn('el.filterByTag is deprecated.');
-        return this.filter(tag);
-    },
+	effect: function(property, options){
+		console.warn('1.1 > 1.2: Element.effect is deprecated. use Element.tween');
+		return new Fx.Tween(this, $extend({property: property}, options));
+	},
 
-    filterByClass: function(className){
-		console.warn('el.filterByClass is deprecated.');
-        return this.filter('.' + className);
-    },
+	effects: function(options){
+		console.warn('1.1 > 1.2: Element.effects is deprecated. use Element.morph');
+		return new Fx.Morph(this, options);
+	},
 
-    filterById: function(id){
-		console.warn('el.filterById is deprecated.');
-        return this.filter('#' + id);
-    },
+	filterByTag: function(tag){
+		console.warn('1.1 > 1.2: Element.filterByTag is deprecated.');
+			return this.filter(tag);
+	},
 
-    filterByAttribute: function(name, operator, value){
-		console.warn('el.filterByAttribute is deprecated.');
-        return this.filter('[' + name + (operator || '') + (value || '') + ']');
-    }
+	filterByClass: function(className){
+		console.warn('1.1 > 1.2: Element.filterByClass is deprecated.');
+		return this.filter('.' + className);
+	},
+
+	filterById: function(id){
+		console.warn('1.1 > 1.2: Element.filterById is deprecated.');
+		return this.filter('#' + id);
+	},
+
+	filterByAttribute: function(name, operator, value){
+		console.warn('1.1 > 1.2: Element.filterByAttribute is deprecated.');
+		return this.filter('[' + name + (operator || '') + (value || '') + ']');
+	}
 
 });
 
-
 window.extend = document.extend = function(properties){
-	console.warn('(window||document).extend is deprecated');
-    for (var property in properties) this[property] = properties[property];
+	console.warn('1.1 > 1.2: (window||document).extend is deprecated');
+	for (var property in properties) this[property] = properties[property];
+};
+
+
+Element.Properties.properties = {
+	
+	set: function(props){
+		console.warn('1.1 > 1.2: Element.set({properties: {}}) is deprecated; instead of properties, just name the values at the root of the object (Element.set({src: url})).');
+		$H(props).each(function(value, property){
+			this.set(property, value);
+		}, this);
+	}
+	
+};
+
+
+Element.Properties.styles = {
+	
+	set: function(styles){
+		if ($type(styles) == 'string' && !styles.contains('{')){
+			this.style.cssText = styles;
+		} else {
+			this.setStyles(styles);
+		}
+	}
+
 };
