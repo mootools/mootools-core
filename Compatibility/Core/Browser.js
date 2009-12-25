851e44a858970258ff562c066d99e295559ee07c
Darren Waddell <fakedarren@Jeffrey.local>
2009-12-25 15:37:14 +0000

    quick and dirty browser checking

--- Compatibility/Core/Browser.js
+++ Compatibility/Core/Browser.js
@@ -2,8 +2,11 @@ window.extend = document.extend = function(properties){
 	for (var property in properties) this[property] = properties[property];
 };
 
-window[Browser.Engine.name] = window[Browser.Engine.name + Browser.Engine.version] = true;
-
-window.ie = window.trident;
-window.ie6 = window.trident4;
-window.ie7 = window.trident5;
+window.ie = Browser.Engine.trident ? true : false;
+window.ie6 = Browser.Engine.trident4 ? true : false;
+window.ie7 = Browser.Engine.trident5 ? true : false;
+window.gecko = Browser.Engine.gecko ? true : false;
+window.webkit = Browser.Engine.webkit ? true : false;
+window.webkit419 = Browser.Engine.webkit419 ? true : false;
+window.webkit420 = Browser.Engine.webkit420 ? true : false;
+window.opera = Browser.Engine.presto ? true : false;
