e1be6665854cd5d6e31c4d45afe70cf809bf9405
Darren Waddell <fakedarren@Jeffrey.local>
2009-12-26 13:47:30 +0000

    formatting

--- Compatibility/Element/Element.Dimensions.js
+++ Compatibility/Element/Element.Dimensions.js
@@ -1,25 +1,40 @@
 (function(){
-	
+
+	var getPosition = Element.prototype.getPosition;
+	var getCoordinates = Element.prototype.getCoordinates;
+
 	function isBody(element){
 		return (/^(?:body|html)$/i).test(element.tagName);
 	};
-	
-	var oldgetSize = Element.prototype.getSize;
+
+	var getSize = Element.prototype.getSize;
 
 	Element.implement({
-		
+	
 		getSize: function(){
-			var size;
-			if (isBody(this)) size =  this.getWindow().getSize();
-			else size = oldgetSize();
-			return {
-				x : size.x,
-				y: size.y,
+			console.warn('1.1 > 1.2: NOTE: getSize is different in 1.2; it no longer returns values for size, scroll, and scrollSize, but instead just returns x/y values for the dimensions of the element.');
+			var size = getSize.apply(this, arguments);
+			return $merge(size, {
 				size: size,
 				scroll: this.getScroll(),
 				scrollSize: this.getScrollSize()
-			};
+			});
+		},
+
+		getPosition: function(relative){
+			if (relative && $type(relative) == "array") {
+				console.warn('1.1 > 1.2: Element.getPosition no longer accepts an array of overflown elements but rather, optionally, a single element to get relative coordinates.');
+			}
+			return getPosition.apply(this, arguments);
+		},
+
+		getCoordinates: function(relative){
+			if (relative && $type(relative) == "array") {
+				console.warn('1.1 > 1.2: Element.getCoordinates no longer accepts an array of overflown elements but rather, optionally, a single element to get relative coordinates.');
+			}
+			return getCoordinates.apply(this, arguments);
 		}
+	
 	});
 
 })();
