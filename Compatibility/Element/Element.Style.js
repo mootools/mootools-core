8bbd4861c09d2e3465b98bf600b0a8faba82b88b
Darren Waddell <fakedarren@Jeffrey.local>
2009-12-26 14:36:31 +0000

    Added legacy .set('styles') support

--- Compatibility/Element/Element.Style.js
+++ Compatibility/Element/Element.Style.js
@@ -5,3 +5,17 @@ Element.implement({
 	}
 
 });
+
+Element.Properties.styles = {
+	
+	set: function(styles){
+		if ($type(styles) == 'string'){
+			styles.split(";").each(function(style){
+				this.setStyle(style.split(":")[0], style.split(":")[1]);
+			}, this);
+		} else {
+			this.setStyles(styles);
+		}
+	}
+	
+};
