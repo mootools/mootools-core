0e18c00b3aff36da29792360de63d2782fcc1f5e
Christoph Pojer <christoph.pojer@gmail.com>
2010-02-12 03:37:01 +0100

    * Possible fix for Array.flatten with Array "subobjects"

--- Source/Types/Array.js
+++ Source/Types/Array.js
@@ -131,7 +131,7 @@ Array.implement({
 		for (var i = 0, l = this.length; i < l; i++){
 			var type = typeOf(this[i]);
 			if (type == 'null') continue;
-			array = array.concat((type == 'array' || type == 'collection' || type == 'arguments') ? Array.flatten(this[i]) : this[i]);
+			array = array.concat((type == 'array' || type == 'collection' || type == 'arguments' || instanceOf(this[i], Array)) ? Array.flatten(this[i]) : this[i]);
 		}
 		return array;
 	},
