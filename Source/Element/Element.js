9efc0113dad9f6cc56ca393fa26bb675fc145e50
Fabio M. Costa <fabiomcosta@gmail.com>
2010-03-26 23:50:28 -0300

    using feature detection for the html getter

--- Source/Element/Element.js
+++ Source/Element/Element.js
@@ -679,6 +679,12 @@ Element.Properties.tag = {
 };
 
 Element.Properties.html = (function(){
+	
+	var tableTest = Function.stab(function(){
+		var table = document.createElement('table');
+		table.innerHTML = '<tr><td></td></tr>';
+	});
+	
 	var wrapper = document.createElement('div');
 
 	var translations = {
@@ -692,7 +698,7 @@ Element.Properties.html = (function(){
 	var html = {
 		set: function(){
 			var html = Array.flatten(arguments).join('');
-			var wrap = Browser.ie && translations[this.get('tag')];
+			var wrap = (!tableTest && translations[this.get('tag')]);
 			if (wrap){
 				var first = wrapper;
 				first.innerHTML = wrap[1] + html + wrap[2];
