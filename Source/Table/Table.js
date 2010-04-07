8a236cb41bd67b00326e332f07ad59f4a51731a5
Valerio Proietti <kamicane@gmail.com>
2010-04-07 10:13:02 +0200

    - Table has now private keys and values.

--- Source/Table/Table.js
+++ Source/Table/Table.js
@@ -10,48 +10,43 @@ provides: Table
 (function(){
 
 var Table = this.Table = function(){
-	this.keys = [];
-	this.values = [];
+
 	this.length = 0;
-};
-
-Table.prototype = {
-
-	set: function(key, value){
-		var index = this.keys.indexOf(key);
+	var keys = [], values = [];
+	
+	this.set = function(key, value){
+		var index = keys.indexOf(key);
 		if (index == -1){
-			var length = this.keys.length;
-			this.keys[length] = key;
-			this.values[length] = value;
+			var length = keys.length;
+			keys[length] = key;
+			values[length] = value;
 			this.length++;
 		} else {
-			this.values[index] = value;
+			values[index] = value;
 		}
 		return this;
-	},
+	};
 
-	get: function(key){
-		var index = this.keys.indexOf(key);
-		return (index == -1) ? null : this.values[index];
-	},
+	this.get = function(key){
+		var index = keys.indexOf(key);
+		return (index == -1) ? null : values[index];
+	};
 
-	erase: function(key){
-		var index = this.keys.indexOf(key);
+	this.erase = function(key){
+		var index = keys.indexOf(key);
 		if (index != -1){
 			this.length--;
-			this.keys.splice(index, 1);
-			return this.values.splice(index, 1)[0];
+			keys.splice(index, 1);
+			return values.splice(index, 1)[0];
 		}
 		return null;
-	},
+	};
 
-	forEach: function(fn, bind){
-		for (var i = 0, l = this.length; i < l; i++) fn.call(bind, this.keys[i], this.values[i], this);
-	}
-
+	this.each = this.forEach = function(fn, bind){
+		for (var i = 0, l = this.length; i < l; i++) fn.call(bind, keys[i], values[i], this);
+	};
+	
 };
-
-Table.prototype.each = Table.prototype.forEach;
 
 if (this.Type) new Type('Table', Table);
 
