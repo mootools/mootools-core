e3a7b0c21ff0106d3a67251e7751c1ce84cff063
Christoph Pojer <christoph.pojer@gmail.com>
2010-07-23 17:56:37 +0200

    * Fixing indentation in DomReady

--- Source/Utilities/DomReady.js
+++ Source/Utilities/DomReady.js
@@ -24,40 +24,40 @@ Element.Events.domready = {
 
 (function(){
 
-	var domready = function(){
-		if (Browser.loaded) return;
-		Browser.loaded = true;
-		window.fireEvent('domready');
-		document.fireEvent('domready');
+var domready = function(){
+	if (Browser.loaded) return;
+	Browser.loaded = true;
+	window.fireEvent('domready');
+	document.fireEvent('domready');
+};
+
+var repeat;
+if (Browser.ie){
+	Element.Events.load = {
+		base: 'load',
+		onAdd: function(fn){
+			if (Browser.loaded) fn.call(this);
+		},
+		condition: function(){
+			domready();
+			return true;
+		}
 	};
-	
-	var repeat;
-	if (Browser.ie){
-		Element.Events.load = {
-			base: 'load',
-			onAdd: function(fn){
-				if (Browser.loaded) fn.call(this);
-			},
-			condition: function(){
-				domready();
-				return true;
-			}
-		};
-		var temp = document.createElement('div');
-		repeat = function(){
-			(Function.attempt(function(){
-				temp.doScroll(); // Technique by Diego Perini
-				return document.id(temp).inject(document.body).set('html', 'temp').dispose();
-			})) ? domready() : repeat.delay(50);
-		};
-		repeat();
-	} else if (Browser.safari && Browser.version < 4){
-		repeat = function(){
-			(['loaded', 'complete'].contains(document.readyState)) ? domready() : repeat.delay(50);
-		};
-		repeat();
-	} else {
-		document.addEvent('DOMContentLoaded', domready);
-	}
+	var temp = document.createElement('div');
+	repeat = function(){
+		(Function.attempt(function(){
+			temp.doScroll(); // Technique by Diego Perini
+			return document.id(temp).inject(document.body).set('html', 'temp').dispose();
+		})) ? domready() : repeat.delay(50);
+	};
+	repeat();
+} else if (Browser.safari && Browser.version < 4){
+	repeat = function(){
+		(['loaded', 'complete'].contains(document.readyState)) ? domready() : repeat.delay(50);
+	};
+	repeat();
+} else {
+	document.addEvent('DOMContentLoaded', domready);
+}
 
 })();
