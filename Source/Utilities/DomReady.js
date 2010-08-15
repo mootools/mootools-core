d5506db103827bd24d82e962652f169cbbc09abe
Thomas Aylott <oblivious+git@subtlegradient.com>
2010-08-15 01:51:35 -0400

    DomReady
    Live test: http://projects.subtlegradient.com/domready/DomReady.test.php
    
    The tests pass in Safari 2.0, Safari 2.0.4, Safari 5 (natch’), IE6 & IE8. It should work on PS3 afaict.
    I haven’t done a really extensive test of all old browsers yet.
    
    This was a completely Test-Driven implementation.
    I didn’t fix any specific bug until I had a failing test case in my php file.
    I also wanted to avoid all userAgent sniffing.
    
    This should work around all the domready issues that I know about.
    
    * a default DOMContentLoaded event
    	(since there’s no harm and no known FT)
    * onreadystatechange event
    	run domready before onload in IE iframes since the doScroll poll doesn’t work
    	potentially useful for some unknown browsers?
    * poll for readyState where onreadystatechange is missing
    	(using the conventional  `‘oneventname’ in document` event feature test)
    * poll for doScroll on a temporary element if the method exists unless window != top
    	(I tested both tmpElement.doScroll and body.doScroll and there is no difference afaict)
    * no bodyIsEditable test since I couldn’t create a failing test case to prove that it’s useful info.
    	What browser might require such a test?
    * the final passing test disables the poll timer and removes all related events to clean up after itself.
    
    CSS Ready is far out of scope for this script.

--- Source/Utilities/DomReady.js
+++ Source/Utilities/DomReady.js
@@ -7,63 +7,95 @@ description: Contains the custom event domready.
 
 license: MIT-style license.
 
-requires: Element.Event
+requires: [Browser, Element, Element.Events]
 
 provides: DomReady
 
 ...
 */
 
-(function(){
+(function(window, document){
 
-var onAdd = function(fn){
-	if (Browser.loaded) fn.call(this);
-};
+var	ready
+,	checks = []
+,	shouldPoll
+,	timer;
 
-var domready = function(){
-	if (Browser.loaded) return;
-	Browser.loaded = true;
-	window.fireEvent('domready');
+function domready(){
+	clearTimeout(timer);
+	if (ready) return;
+	Browser.loaded = ready = true;
+	document.removeListener('DOMContentLoaded', domready);
+	document.removeListener('readystatechange', check);
+	
 	document.fireEvent('domready');
+	window.fireEvent('domready');
 };
 
-Element.Events.domready = {
-	onAdd: onAdd
-};
+document.addListener('DOMContentLoaded', domready);
 
-var repeat;
-if (Browser.ie){
-	Element.Events.load = {
-		base: 'load',
-		onAdd: onAdd,
-		condition: function(){
-			domready();
+
+function check(){
+	var i = checks.length;
+	while (i--) if (checks[i]()){
+		domready();
+		return true;
+	}
+	return false;
+}
+
+function poll(){
+	clearTimeout(timer);
+	if (!check()) timer = setTimeout(poll, 10);
+}
+
+
+// doScroll technique by Diego Perini http://javascript.nwbox.com/IEContentLoaded/
+var testElement = document.createElement('div');
+if (testElement.doScroll && this.window == this.top){
+	checks.push(function(){
+		try {
+			testElement.doScroll();
 			return true;
 		}
-	};
-
-	var temp = document.createElement('div');
-	var check = function(){
-		temp.doScroll(); // Technique by Diego Perini
-		return document.id(temp).inject(document.body).set('html', 'temp').dispose();
-	};
-	repeat = function(){
-		if (Function.attempt(check)) domready();
-		else repeat.delay(50);
-	};
-	repeat();
-	return;
+		catch (e){
+			return false;
+		}
+	});
+	shouldPoll = true;
 }
 
-if (Browser.safari && Browser.version < 4){
-	repeat = function(){
-		if (['loaded', 'complete'].contains(document.readyState)) domready();
-		else repeat.delay(50);
-	};
-	repeat();
-	return;
-}
 
-document.addEvent('DOMContentLoaded', domready);
+var stateReady = { loaded:1, complete:1 };
+if (document.readyState) checks.push(function(){
+	return stateReady[document.readyState];
+});
 
-})();
+
+if ('onreadystatechange' in document) document.addListener('readystatechange', check);
+else shouldPoll = true;
+
+
+if (shouldPoll) poll();
+
+
+Element.Events.domready = {
+	onAdd: function(fn){
+		if (ready) fn.call(this);
+	}
+};
+
+// Make sure that domready fires before load
+Element.Events.load = {
+	base: 'load',
+	onAdd: Element.Events.domready.onAdd,
+	condition: function(){
+		domready();
+		return true;
+	}
+};
+window.addEvent('load',function(){
+	delete Element.Events.load;
+});
+
+})(window, document);
