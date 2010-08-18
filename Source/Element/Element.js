2a265b419a7488d0a77184b52f83593711ad2406
Fabio M. Costa <fabiomcosta@gmail.com>
2010-08-18 00:44:16 -0300

    Looks like when we touch the Element.prototype before a css behavior the binded (this) object gets broken, breaking the behavior and the browser.

--- Source/Element/Element.js
+++ Source/Element/Element.js
@@ -14,8 +14,7 @@ provides: [Element, Elements, $, $$, Iframe]
 ...
 */
 
-// it needs to be this.Element cause IE8 erases the Element Object while pre-processing this script
-this.Element = function(tag, props){
+var Element = function(tag, props){
 	var konstructor = Element.Constructors[tag];
 	if (konstructor) return konstructor(props);
 	if (typeof tag != 'string') return document.id(tag).set(props);
