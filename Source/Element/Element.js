d1d503ceff536d95f9b7517260a8fff82ecba1c5
Thomas Aylott <oblivious+git@subtlegradient.com>
2010-09-08 23:25:57 -0400

    FIXED [#977 state:closed] [#968 state:closed] -- `new Element('input',  {name, type})` now works in IE 6-7 again

--- Source/Element/Element.js
+++ Source/Element/Element.js
@@ -144,12 +144,46 @@ new Type('Elements', Elements).implement({
 
 Array.mirror(Elements);
 
+(function(){
+
+/*<ltIE8>*/
+var createElementAcceptsHTML;
+try {
+	var x = document.createElement('<input name=x>');
+	createElementAcceptsHTML = x.name == 'x';
+} catch(e){}
+
+var escapeQuotes = function(html){
+	return ('' + html).replace(/&/g,'&amp;').replace(/"/g,'&quot;');
+};
+/*</ltIE8>*/
+
 Document.implement({
 
 	newElement: function(tag, props){
 		if (props && props.checked != null) props.defaultChecked = props.checked;
+		/*<ltIE8>*/// Fix for readonly name and type properties in IE < 8
+		if (props && createElementAcceptsHTML){
+			tag = '<' + tag;
+			if (props.name){
+				tag += ' name="' + escapeQuotes(props.name) + '"';
+				delete props.name;
+			}
+			if (props.type){
+				tag += ' type="' + escapeQuotes(props.type) + '"';
+				delete props.type;
+			}
+			tag += '>';
+		}
+		/*</ltIE8>*/
 		return this.id(this.createElement(tag)).set(props);
-	},
+	}
+
+});
+
+})();
+
+Document.implement({
 
 	newTextNode: function(text){
 		return this.createTextNode(text);
