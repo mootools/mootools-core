1a8245a0c37094ce019f01bad1679e5b7cf2203b
Thomas Aylott <oblivious+github@subtleGradient.com>
2010-09-09 15:36:20 -0700

    Cleanup the code a wee bit. Thanks CPojer

--- Source/Element/Element.js
+++ Source/Element/Element.js
@@ -150,7 +150,7 @@ Array.mirror(Elements);
 var createElementAcceptsHTML;
 try {
 	var x = document.createElement('<input name=x>');
-	createElementAcceptsHTML = x.name == 'x';
+	createElementAcceptsHTML = (x.name == 'x');
 } catch(e){}
 
 var escapeQuotes = function(html){
@@ -163,17 +163,13 @@ Document.implement({
 	newElement: function(tag, props){
 		if (props && props.checked != null) props.defaultChecked = props.checked;
 		/*<ltIE8>*/// Fix for readonly name and type properties in IE < 8
-		if (props && createElementAcceptsHTML){
+		if (createElementAcceptsHTML && props){
 			tag = '<' + tag;
-			if (props.name){
-				tag += ' name="' + escapeQuotes(props.name) + '"';
-				delete props.name;
-			}
-			if (props.type){
-				tag += ' type="' + escapeQuotes(props.type) + '"';
-				delete props.type;
-			}
+			if (props.name) tag += ' name="' + escapeQuotes(props.name) + '"';
+			if (props.type) tag += ' type="' + escapeQuotes(props.type) + '"';
 			tag += '>';
+			delete props.name;
+			delete props.type;
 		}
 		/*</ltIE8>*/
 		return this.id(this.createElement(tag)).set(props);
