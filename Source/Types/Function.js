ba96400aa052cdd1857ba919de2b1673f6913bcf
Valerio Proietti <kamicane@gmail.com>
2010-09-21 20:28:33 +0200

    I am fairly confident that this will have to be updated as well.

--- Source/Types/Function.js
+++ Source/Types/Function.js
@@ -7,53 +7,42 @@ provides: Function
 ...
 */
 
-Function.extend({
-
-	clear: function(timer){
-		clearInterval(timer);
-		clearTimeout(timer);
-		return null;
-	},
-
-	stab: function(){
-		for (var i = 0, l = arguments.length; i < l; i++){
-			try {
-				return arguments[i]();
-			} catch (e){}
-		}
-		return null;
+Function.extend('attempt', function(){
+	for (var i = 0, l = arguments.length; i < l; i++){
+		try {
+			return arguments[i]();
+		} catch (e){}
 	}
-
+	return null;
 });
 
 Function.implement({
+	
+	bind: function(bind){
+		var self = this, args = Array.slice(arguments, 1);
+		return function(){
+			return self.apply(bind, (args.length) ? args : arguments);
+		};
+	},
 
 	attempt: function(args, bind){
 		try {
-			return this.apply(bind, Array.from(args));
+			return this.run(args, bind);
 		} catch (e){
 			return null;
 		}
 	},
 
-	bind: function(bind, args){
-		var self = this;
-		if (args != null) args = Array.from(args);
-		return function(){
-			return self.apply(bind, args || arguments);
-		};
-	},
-
 	delay: function(delay, bind, args){
-		return setTimeout(this.bind(bind, args), delay);
+		return setTimeout(this.pass(args, bind), delay);
 	},
 
 	pass: function(args, bind){
-		return this.bind(bind, args);
+		return this.bind.apply(this, [bind].concat(args));
 	},
 
 	periodical: function(periodical, bind, args){
-		return setInterval(this.bind(bind, args), periodical);
+		return setInterval(this.pass(args, bind), periodical);
 	},
 
 	run: function(args, bind){
