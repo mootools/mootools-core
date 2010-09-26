be5304085f2fa78e9122b67f638f058d1d2557fd
Christoph Pojer <christoph.pojer@gmail.com>
2010-09-26 21:19:20 +0200

    * Adding .protect to Elements.append

--- Source/Element/Element.js
+++ Source/Element/Element.js
@@ -146,7 +146,7 @@ new Type('Elements', Elements).implement({
 	append: function(collection){
 		for (var i = 0, l = collection.length; i < l; i++) this.push(collection[i]);
 		return this;
-	},
+	}.protect(),
 
 	empty: function(){
 		while (this.length) delete this[--this.length];
