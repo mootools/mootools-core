024dd89c61e9b6b1b645ce16315a14352b289c49
Christoph Pojer <christoph.pojer@gmail.com>
2010-11-09 19:03:24 +0100

    * Minor code optimization

--- Source/Class/Class.Extras.js
+++ Source/Class/Class.Extras.js
@@ -108,8 +108,7 @@ this.Options = new Class({
 
 	setOptions: function(){
 		var options = this.options = Object.merge.apply(null, [{}, this.options].append(arguments));
-		if (!this.addEvent) return this;
-		for (var option in options){
+		if (this.addEvent) for (var option in options){
 			if (typeOf(options[option]) != 'function' || !(/^on[A-Z]/).test(option)) continue;
 			this.addEvent(option, options[option]);
 			delete options[option];
