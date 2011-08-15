d47b0edc97392c18b730bcd01776d262387f1c70
Daniel Buchner <danieljb2@gmail.com>
2011-08-15 13:11:37 -0700

    moved Element.Events.change custom event to seperate file, added RM docs and entry to package.yml

---
+++ Docs/Element/Element.Event.Change.md
@@ -0,0 +1,20 @@
+Type: Event {#Event}
+==========================
+
+- This custom change event ensures the onchange event behaves the same across browsers.
+- The change event is strictly intended for elements that support the native onchange event.
+- For further information on cross-browser issues with the change event, visit Peter-Paul Koch's Quirksmode change event page: http://www.quirksmode.org/dom/events/change.html
+
+Element.Events.change
+----------------------
+
+### Event: change {#Element-Events:change}
+
+This event fires immediatly after an element's value changes
+
+#### Examples:
+	// Adding a change event directly to an element
+	$('myElement').addEvent('change', myFunction);
+	
+	// Delegating a change event to many elements
+	$('myContainer').addEvent('change:relay(input)', myFunction);
---
+++ Source/Element/Element.Event.Change.js
@@ -0,0 +1,74 @@
+/*
+---
+
+name: Element.Event.Change
+
+description: Contains Element methods for normalizing change events across browsers.
+
+license: MIT-style license.
+
+requires: [Element, Event]
+
+provides: Element.Event.Change
+
+...
+*/
+
+(function(){
+
+var hasListener = window.addEventListener;
+var getValue = function(element){
+	var type = element.get('type');
+	return element[type == 'radio' || type == 'checkbox' ? 'checked' : 'value'];
+}
+var storeChange = function(event){
+	event.target.store('$change', {
+		type: event.type,
+		value: getValue(event.target)
+	});
+};
+var hasChanged = function(element){
+	var value =  element.retrieve('$change').value;
+	return (value != null && value != getValue(element));
+};
+
+Element.Events.keychange = {
+	base: 'keyup',
+	condition: function(event){
+		var target = event.target;
+		if (!hasChanged(target)) return false;
+		if (target.get('tag') == 'select') return true;
+		if (!hasListener) switch(event.key){
+			case 'up': case 'down': case 'left': case 'right': return target.get('type') == 'radio';
+			case 'space': return target.get('type') == 'checkbox';
+		}
+	}
+};
+
+Element.Events.change = {
+	base: function(){
+		var type = this.get('type');
+		return (!hasListener && (type == 'checkbox' || type == 'radio')) ? 'click' : 'change';
+	},
+	condition: function(event){
+		var target = event.target;
+		if (hasListener && event.type == 'change' && target.get('tag') == 'select' && target.retrieve('$change').type == 'keydown') return false;
+		return target.get('type') == 'radio' ? (event.type == 'keyup' ? !target.checked : target.checked) : true;
+	},
+	onAdd: function(fn){
+		this.addEvents({
+			keychange: fn,
+			focus: storeChange,
+			keydown: storeChange
+		});
+	},
+	onRemove: function(fn){
+		this.removeEvents({
+			keychange: fn,
+			focus: storeChange,
+			keydown: storeChange
+		});
+	}
+}
+
+})();
