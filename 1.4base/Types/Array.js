9c9bdb993b41a68f66aca8603aa0521fb8266277
Arian <arian@aryweb.nl>
2011-08-19 02:33:17 +0200

    Specs for weird thisArgs for ES5 Array methods. pull request: https://github.com/mootools/mootools-core/pull/2035

--- 1.4base/Types/Array.js
+++ 1.4base/Types/Array.js
@@ -9,6 +9,21 @@ describe('Array', function(){
 			}).length).toEqual(4);
 		});
 
+		it('shoud return an empty array when the thisArg does not has a length property', function(){
+			expect([].map.call({}, function(){
+				return 1;
+			})).toEqual([]);
+		});
+
+	});
+
+	it('should accept thisArgs without length property', function(){
+		var object = {}, fn = function(){};
+		expect([].every.call(object, fn)).toBe(true);
+		expect([].filter.call(object, fn)).toEqual([]);
+		expect([].indexOf.call(object)).toEqual(-1);
+		expect([].map.call(object, fn)).toEqual([]);
+		expect([].some.call(object, fn)).toBe(false);
 	});
 
 });
