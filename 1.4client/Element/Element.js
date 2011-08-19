43ab3a237f3c41b75c7df9bee148850ee286e8ff
Garrick Cheung <garrick@garrickcheung.com>
2011-08-18 19:07:09 -0700

    Adding Video/Audio set/get attributes specs for loop, controls, and autoplay ONLY if browser supports video.

--- 1.4client/Element/Element.js
+++ 1.4client/Element/Element.js
@@ -6,77 +6,98 @@ requires: [Core/Element.Event]
 provides: [Element.Event.Specs]
 ...
 */
+describe('Element', function(){
 
-describe('Element.getProperty', function(){
+	describe('Element.getProperty', function(){
 
-	it('should get the attrubte of a form when the form has an input with as ID the attribute name', function(){
-		var div = new Element('div');
-		div.innerHTML = '<form action="s"><input id="action"></form>';
-		expect($(div.firstChild).getProperty('action')).toEqual('s');
-	});
-
-});
-
-describe('Element.get', function(){
-
-	describe('value', function(){
-
-		it('should get the value of a option element when it does not have the value attribute', function(){
-			var select = new Element('select').set('html', '<option>s</option>');
-			expect(select.getElement('option').get('value')).toEqual('s');
-		});
-
-		it('should return the text of the selected option for a select element', function(){
-			var form = new Element('form');
-			form.set('html', '<select>\
-				<option>value 1</option>\
-				<option>value 2</option>\
-				<option selected>value 3</option>\
-				<option>value 4</option>\
-				</select>');
-			expect(form.getElement('select').get('value')).toEqual('value 3');
-		});
-
-		it('should return the text of the selected option for a multiple select element', function(){
-			var form = new Element('form');
-			form.set('html', '<select multiple>\
-				<option>value 1</option>\
-				<option selected>value 2</option>\
-				<option selected>value 3</option>\
-				<option>value 4</option>\
-				</select>');
-			expect(form.getElement('select').get('value')).toEqual('value 2');
-		});
-
-		it('should return the text of the first option of aselect element', function(){
-			var form = new Element('form');
-			form.set('html', '<select>\
-				<option>value 1</option>\
-				<option>value 2</option>\
-				</select>');
-			expect(form.getElement('select').get('value')).toEqual('value 1');
-		});
-
-		it('should return value of a select element', function(){
-			var form = new Element('form');
-			form.set('html', '<select multiple>\
-				<option value="one">value 1</option>\
-				<option selected value="two">value 2</option>\
-				</select>');
-			expect(form.getElement('select').get('value')).toEqual('two');
+		it('should get the attrubte of a form when the form has an input with as ID the attribute name', function(){
+			var div = new Element('div');
+			div.innerHTML = '<form action="s"><input id="action"></form>';
+			expect($(div.firstChild).getProperty('action')).toEqual('s');
 		});
 
 	});
 
-});
+	describe('Element.get', function(){
 
-describe('tabIndex', function(){
+		describe('value', function(){
 
-	it('should get and set the correct tabIndex', function(){
-		var div = document.createElement('div');
-		div.innerHTML = '<input tabindex="2">';
-		expect($(div.firstChild).get('tabindex')).toEqual(2);
-		expect($(div.firstChild).set('tabindex', 3).get('tabindex')).toEqual(3);
+			it('should get the value of a option element when it does not have the value attribute', function(){
+				var select = new Element('select').set('html', '<option>s</option>');
+				expect(select.getElement('option').get('value')).toEqual('s');
+			});
+
+			it('should return the text of the selected option for a select element', function(){
+				var form = new Element('form');
+				form.set('html', '<select>\
+					<option>value 1</option>\
+					<option>value 2</option>\
+					<option selected>value 3</option>\
+					<option>value 4</option>\
+					</select>');
+				expect(form.getElement('select').get('value')).toEqual('value 3');
+			});
+
+			it('should return the text of the selected option for a multiple select element', function(){
+				var form = new Element('form');
+				form.set('html', '<select multiple>\
+					<option>value 1</option>\
+					<option selected>value 2</option>\
+					<option selected>value 3</option>\
+					<option>value 4</option>\
+					</select>');
+				expect(form.getElement('select').get('value')).toEqual('value 2');
+			});
+
+			it('should return the text of the first option of aselect element', function(){
+				var form = new Element('form');
+				form.set('html', '<select>\
+					<option>value 1</option>\
+					<option>value 2</option>\
+					</select>');
+				expect(form.getElement('select').get('value')).toEqual('value 1');
+			});
+
+			it('should return value of a select element', function(){
+				var form = new Element('form');
+				form.set('html', '<select multiple>\
+					<option value="one">value 1</option>\
+					<option selected value="two">value 2</option>\
+					</select>');
+				expect(form.getElement('select').get('value')).toEqual('two');
+			});
+
+		});
+
 	});
+
+	describe('tabIndex', function(){
+
+		it('should get and set the correct tabIndex', function(){
+			var div = document.createElement('div');
+			div.innerHTML = '<input tabindex="2">';
+			expect($(div.firstChild).get('tabindex')).toEqual(2);
+			expect($(div.firstChild).set('tabindex', 3).get('tabindex')).toEqual(3);
+		});
+
+	});
+
+	if (document.createElement('video').canPlayType){
+		describe('Video/Audio loop, controls, and autoplay set/get attributes', function(){
+
+			it('should set/get the boolean value of loop, controls, and autoplay', function(){
+				var div = new Element('div', {html: '<video loop controls autoplay>'}),
+					video = div.getElement('video');
+
+				expect(video.getProperty('loop')).toBe(true);
+				expect(video.setProperty('loop', false).getProperty('loop')).toBe(false);
+				expect(video.getProperty('controls')).toBe(true);
+				expect(video.setProperty('controls', false).getProperty('controls')).toBe(false);
+				expect(video.getProperty('autoplay')).toBe(true);
+				expect(video.setProperty('autoplay', false).getProperty('autoplay')).toBe(false);
+			});
+
+		});
+	}
 
 });
