0f236376bd6ae5c80abb81c79cbc2faa3817f629
Garrick Cheung <garrick@garrickcheung.com>
2011-08-23 12:44:31 -0700

    Removing "loop" from bools because other browsers don't seem to be implementing this properly.

--- Source/Element/Element.js
+++ Source/Element/Element.js
@@ -535,8 +535,7 @@ Object.forEach(properties, function(real, key){
 var bools = [
 	'compact', 'nowrap', 'ismap', 'declare', 'noshade', 'checked',
 	'disabled', 'readOnly', 'multiple', 'selected', 'noresize',
-	'defer', 'defaultChecked', 'autofocus', 'controls', 'autoplay',
-	'loop'
+	'defer', 'defaultChecked', 'autofocus', 'controls', 'autoplay'
 ];
 
 var booleans = {};
