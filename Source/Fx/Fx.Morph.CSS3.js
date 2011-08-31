62725d532a36856ae538b49b49c1d7d26decedb7
Fred Cox <mcfedr@gmail.com>
2011-09-01 00:05:08 +0300

    Add CSS3 support inplace of javascript animation for morph and tween

---
+++ Source/Fx/Fx.Morph.CSS3.js
@@ -0,0 +1,69 @@
+/*
+---
+name: Fx.Tween.CSS3
+script: Fx.Tween.CSS3.js
+license: MIT-style license.
+description: Provides a CSS3 implementaton of Fx.Morph
+copyright: Copyright (c) 2010, Dipl.-Ing. (FH) André Fiedler <kontakt at visualdrugs dot net>, based on code by eskimoblood (mootools users group)
+copyright: Copyright (c) 2011 Fred Cox mcfedr@gmail.com
+authors: [Fred Cox, André Fiedler, eskimoblood]
+
+requires: [Fx.CSS3Funcs, Fx.Morph]
+
+provides: [Fx.Morph.CSS3]
+...
+*/
+
+var morphCSS2 = Fx.Morph;
+
+Fx.Morph = new Class({
+	
+	Extends: morphCSS2,
+	
+	check: function(properties){
+		return (this.css3Supported && !this.boundComplete && animatable.containsArray(Object.keys(properties))) || this.parent();
+	},
+
+	start: function(properties){
+		if (this.css3Supported){
+			if (!this.check(properties)) return this;
+			if (typeof properties == 'string') properties = this.search(properties);
+			var from = {}, to = {};
+			for (var p in properties){
+				var parsed = this.prepare(this.element, p, properties[p]);
+				from[p] = parsed.from;
+				to[p] = parsed.to;
+			}
+			var incomplete = Object.map(properties, function() { return false; });
+			this.boundComplete = function(event){
+				incomplete[event.getPropertyName()] = true;
+				if(Object.every(incomplete, function(v) { return v; })) {
+					this.element.removeEvent('transitionend', this.boundComplete);
+					this.boundComplete = null;
+					this.fireEvent('complete', this);
+				}
+			}.bind(this);
+			
+			this.element.addEvent('transitionend', this.boundComplete);
+			var trans = function(){
+				var transStyles = {};
+				transStyles[this.transitionProperty] = Object.keys(properties).reduce(function(a, b) { return a + ', '  + b; });
+				transStyles[this.transitionDuration] = this.options.duration + 'ms';
+				transStyles[this.transitionTimingFunction] = 'cubic-bezier(' + transitionTimings[this.options.transition] + ')';
+				this.element.setStyles(transStyles);
+				this.set(this.compute(from, to, 1));
+			}.bind(this);
+			this.element.setStyle(this.transitionProperty, 'none');
+			this.set(this.compute(from, to, 0));
+			trans.delay(0.1);
+			this.fireEvent('start', this);
+			return this;
+		}
+		return this.parent(properties);
+	}
+});
+
+Fx.Morph.implement(Fx.CSS3Funcs);
+
+Fx.Morph.CSS2 = morphCSS2;
+Fx.Morph.CSS3 = Fx.Morph;
---
+++ Source/Fx/Fx.Tween.CSS3.js
@@ -0,0 +1,63 @@
+/*
+---
+name: Fx.Tween.CSS3
+script: Fx.Tween.CSS3.js
+license: MIT-style license.
+description: Provides a CSS3 implementaton of Fx.Tween
+copyright: Copyright (c) 2010, Dipl.-Ing. (FH) André Fiedler <kontakt at visualdrugs dot net>, based on code by eskimoblood (mootools users group)
+copyright: Copyright (c) 2011 Fred Cox mcfedr@gmail.com
+authors: [Fred Cox, André Fiedler, eskimoblood]
+
+requires: [Fx.CSS3Funcs, Fx.Tween]
+
+provides: [Fx.Tween.CSS3]
+...
+*/
+
+var tweenCSS2 = Fx.Tween;
+
+Fx.Tween = new Class({
+	
+	Extends: tweenCSS2,
+	
+	check: function(property){
+		return (this.css3Supported && !this.boundComplete && animatable.contains(property)) || this.parent();
+	},
+
+	start: function(property, from, to){
+		if (this.css3Supported){
+			if (!this.check(property, from, to)) return this;
+			var args = Array.flatten(arguments);
+			this.property = this.options.property || args.shift();
+			var parsed = this.prepare(this.element, this.property, args);
+			this.from = parsed.from;
+			this.to = parsed.to;
+			this.boundComplete = function(event){
+				if (event.getPropertyName() == this.property){
+					this.element.removeEvent('transitionend', this.boundComplete);
+					this.boundComplete = null;
+					this.fireEvent('complete', this);
+				}
+			}.bind(this);
+			this.element.addEvent('transitionend', this.boundComplete);
+			var trans = function(){
+				this.element.setStyle(this.transition, this.property + ' ' + this.options.duration + 'ms cubic-bezier(' + transitionTimings[this.options.transition] + ')');
+				this.set(this.compute(parsed.from, parsed.to, 1));
+			}.bind(this);
+			if (args[1]){
+				this.element.setStyle(this.transition, 'none');
+				this.set(this.compute(parsed.from, parsed.to, 0));
+				trans.delay(0.1);
+			} else
+				trans();
+            this.fireEvent('start', this);
+			return this;
+		}
+		return this.parent(property, from, to);
+	},
+});
+
+Fx.Tween.implement(Fx.CSS3Funcs);
+
+Fx.Tween.CSS2 = tweenCSS2;
+Fx.Tween.CSS3 = Fx.Tween;
