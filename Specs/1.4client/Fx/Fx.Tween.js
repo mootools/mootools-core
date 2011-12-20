19599597c307ab4aaa0c927292173f889b38d2b6
Olmo Maldonado <olmo.maldonado@gmail.com>
2011-12-20 09:18:28 -0600

    Adds coverage for fade(0, 1) usage.
    PASSED: IE6-9; FF3-5, 8, 11; Chrome latest; Safari 5; Opera 11

--- Specs/1.4client/Fx/Fx.Tween.js
+++ Specs/1.4client/Fx/Fx.Tween.js
@@ -41,6 +41,22 @@ describe('Fx.Tween', function(){
 
 		});
 
+		it('should accept the old style arguments (0, 1)', function(){
+
+			var element = new Element('div');
+			element.fade(1, 0);
+
+			var tween = element.get('tween');
+
+			expect(tween.from[0].value).toEqual(1);
+			expect(tween.to[0].value).toEqual(0);
+
+			this.clock.tick(1000);
+
+			expect(element.getStyle('opacity')).toEqual(0);
+
+		});
+
 	});
 
 });
