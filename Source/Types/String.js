2bdbd434e272def2085df7988958e315d329cb02
unknown <dbuchner@dbuchner-07832.(none)>
2012-01-09 08:04:24 -0800

    String.substitute now uses Object.get and is capable of handling replace matches that are path strings

--- Source/Types/String.js
+++ Source/Types/String.js
@@ -74,8 +74,7 @@ String.implement({
 
 	substitute: function(object, regexp){
 		return String(this).replace(regexp || (/\\?\{([^{}]+)\}/g), function(match, name){
-			if (match.charAt(0) == '\\') return match.slice(1);
-			return (object[name] != null) ? object[name] : '';
+			return (match.charAt(0) == '\\') ? match.slice(1) : (Object.get(object, name) || '');
 		});
 	}
 
