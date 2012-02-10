1d25758f1b4627fe8f3f80081fde082c2aa89710
Henning Leutz <henbug@seaofsin.de>
2012-02-10 15:42:14 +0100

    Browser support for new chrome android version and HPTouchpad; Fix for the version number (no parseFloat) because some Browsersversions are x.x.x.x

--- Source/Browser/Browser.js
+++ Source/Browser/Browser.js
@@ -19,9 +19,9 @@ provides: [Browser, Window, Document]
 var document = this.document;
 var window = document.window = this;
 
-var ua = navigator.userAgent.toLowerCase(),
+var ua = navigator.userAgent.toLowerCase().replace('hpwos', 'webos').replace('crmo', 'chrome'),
 	platform = navigator.platform.toLowerCase(),
-	UA = ua.match(/(opera|ie|firefox|chrome|version)[\s\/:]([\w\d\.]+)?.*?(safari|version[\s\/:]([\w\d\.]+)|$)/) || [null, 'unknown', 0],
+	UA = ua.match(/(opera|ie|firefox|chrome|webos|version)[\s\/:]([\w\d\.]+)?.*?(safari|version[\s\/:]([\w\d\.]+)|$)/) || [null, 'unknown', 0],
 	mode = UA[1] == 'ie' && document.documentMode;
 
 var Browser = this.Browser = {
@@ -30,7 +30,7 @@ var Browser = this.Browser = {
 
 	name: (UA[1] == 'version') ? UA[3] : UA[1],
 
-	version: mode || parseFloat((UA[1] == 'opera' && UA[4]) ? UA[4] : UA[2]),
+	version: mode || ((UA[1] == 'opera' && UA[4]) ? UA[4] : UA[2]),
 
 	Platform: {
 		name: ua.match(/ip(?:ad|od|hone)/) ? 'ios' : (ua.match(/(?:webos|android)/) || platform.match(/mac|win|linux/) || ['other'])[0]
