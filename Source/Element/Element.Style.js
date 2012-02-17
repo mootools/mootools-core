92b50e4533ce099b137417f59b4c903a63791d46
Olmo Maldonado <olmo.maldonado@gmail.com>
2012-02-17 11:18:10 -0600

    Element.Style.js: if opacity is greater than 1, reset opacity.

--- Source/Element/Element.Style.js
+++ Source/Element/Element.Style.js
@@ -45,7 +45,7 @@ var setOpacity = (hasOpacity ? function(element, opacity){
 } : (hasFilter ? function(element, opacity){
 	var style = element.style;
 	if (!element.currentStyle || !element.currentStyle.hasLayout) style.zoom = 1;
-	if (opacity == null || opacity == 1) opacity = '';
+	if (opacity == null /*<ltIE9>*/ || Math.min(opacity, 1) == 1 /*</ltIE9>*/) opacity = '';
 	else opacity = 'alpha(opacity=' + (opacity * 100).limit(0, 100).round() + ')';
 	var filter = style.filter || element.getComputedStyle('filter') || '';
 	style.filter = reAlpha.test(filter) ? filter.replace(reAlpha, opacity) : filter + opacity;
