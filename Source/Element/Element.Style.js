953d6a7ab3d76b9a3377b993e18af21a7f4c51bb
Arian <arian@aryweb.nl>
2012-02-20 18:56:34 +0100

    Fixes getStyle() for margins with percentages in Opera

--- Source/Element/Element.Style.js
+++ Source/Element/Element.Style.js
@@ -120,7 +120,7 @@ Element.implement({
 			var color = result.match(/rgba?\([\d\s,]+\)/);
 			if (color) result = result.replace(color[0], color[0].rgbToHex());
 		}
-		if (Browser.ie && isNaN(parseFloat(result))){
+		if (Browser.opera || (Browser.ie && isNaN(parseFloat(result)))){
 			if ((/^(height|width)$/).test(property)){
 				var values = (property == 'width') ? ['left', 'right'] : ['top', 'bottom'], size = 0;
 				values.each(function(value){
@@ -128,8 +128,7 @@ Element.implement({
 				}, this);
 				return this['offset' + property.capitalize()] - size + 'px';
 			}
-			if (Browser.opera && String(result).indexOf('px') != -1) return result;
-			if ((/^border(.+)Width|margin|padding/).test(property)) return '0px';
+			if (!Browser.opera && (/^border(.+)Width|margin|padding/).test(property)) return '0px';
 		}
 		return result;
 	},
