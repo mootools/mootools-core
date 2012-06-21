4d71a964990178027a4d772e89354e45e07c27e9
DimitarChristoff <christoff@gmail.com>
2012-06-21 10:51:51 +0100

    taking coersion to string out of the loop for setting value of selects in ielt9

--- Source/Element/Element.js
+++ Source/Element/Element.js
@@ -1017,14 +1017,15 @@ testForm.innerHTML = '<select><option>s</option></select>';
 if (testForm.firstChild.value != 's') Element.Properties.value = {
 
 	set: function(value){
-		var tag = this.get('tag');
+        var tag = this.get('tag');
 		if (tag != 'select') return this.setProperty('value', value);
 		var options = this.getElements('option');
-		for (var i = 0; i < options.length; i++){
+        value = String(value);
+        for (var i = 0; i < options.length; i++){
 			var option = options[i],
 				attr = option.getAttributeNode('value'),
 				optionValue = (attr && attr.specified) ? option.value : option.get('text');
-			if (optionValue === String(value)) return option.selected = true;
+			if (optionValue === value) return option.selected = true;
 		}
 	},
 
