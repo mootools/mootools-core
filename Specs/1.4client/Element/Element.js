afa55d12c42c2308ac13ea928b0176bf29529c30
DimitarChristoff <christoff@gmail.com>
2012-06-21 10:33:32 +0100

    fix for #2359 - IELT9 select.set('value', 0) with option with value='' was picking wrong one due to loose typing

--- Specs/1.4client/Element/Element.js
+++ Specs/1.4client/Element/Element.js
@@ -69,6 +69,16 @@ describe('Element', function(){
 				expect(new Element('input', {value: 0}).get('value')).toEqual('0');
 			});
 
+            it('should set the selected option for a select element to matching string w/o falsy matches', function(){
+                var form = new Element('form');
+                form.set('html', '<select>\
+                    <option value="">no value</option>\
+					<option value="0">value 0</option>\
+					<option value="1">value 1</option>\
+					</select>');
+                expect(form.getElement('select').set('value', 0).get('value')).toEqual('0');
+            });
+
 		});
 
 		describe('type', function(){
