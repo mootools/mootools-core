921bb0b6581447edf1504a81005a7b7d5bda388a
Daniel Beardsley <daniel@ifixit.com>
2012-07-18 09:02:06 -0700

    Fix a bug: Events broken with prototype usage
    It was previously impossible to use the Events class
    as part of the normal Javascript prototype hierarchy.
    The '$events' property was defined on the prototype
    itself which meant that code like the below would
    produce unexepected results.
    
        function Thing() {}
        Thing.prototype = new Events();
    
    All instances of Thing would end up sharing the same
    object in their '$events' property because it was
    defined on the prototype.  Fixing this usage (and any
    usage outside Mootools Class system) is simple.
    
    This adds appropriate tests to highlight the problem
    and the changes nevessary to overcome it.

--- Specs/1.3client/Class/Class.Extras.js
+++ Specs/1.3client/Class/Class.Extras.js
@@ -167,8 +167,29 @@ var runEventSpecs = function(type, create){
 
 			object[fire]('event');
 			expect(methods).toEqual([1, 2, 3, 3]);
-		}
+		},
 
+		'should have isolated handlers collections': function(){
+			var object2 = create();
+			var object1 = create();
+
+			var methods = [];
+
+			var one = function(){
+				methods.push(1);
+			};
+			var two = function(){
+				methods.push(2);
+			};
+
+			object1.addEvent('event', one);
+			object2.addEvent('event', two);
+
+			object1[fire]('event');
+			expect(methods).toEqual([1]);
+			object2[fire]('event');
+			expect(methods).toEqual([1, 2]);
+		}
 	});
 };
 
@@ -178,6 +199,12 @@ runEventSpecs('mixin', function(){
 
 runEventSpecs('element', function(){
 	return new Element('div');
+});
+
+function TestConstructor () {}
+TestConstructor.prototype = new Events();
+runEventSpecs('prototype', function(){
+	return new TestConstructor;
 });
 
 })();
