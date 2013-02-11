6345deee551504cedd00546509c325dc064ab44b
Arian Stolwijk <stolwijk.arian@gmail.com>
2013-02-11 23:25:11 +0100

    Fixes #2443 - Fix IE9 which trowed an error when setting an input type to "email"

--- Specs/1.5client/Element/Element.js
+++ Specs/1.5client/Element/Element.js
@@ -3,7 +3,7 @@ describe('creating new Elements', function(){
 
 	it('should create an element with type="email"', function(){
 		var el = new Element('input', {type: 'email'});
-		expect(el.get('type')).toBe('email');
+		expect(el.get('type')).toMatch(/email|text/);
 	});
 
 });
