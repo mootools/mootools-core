2d057ba143906547851d6b00eaea5e6728321526
Arian Stolwijk <stolwijk.arian@gmail.com>
2013-02-11 14:01:21 +0100

    Fixes #2367, closes #2467 - Use the check when using delegation for mouseenter.
    Since 1.4.2 the native mouseenter/mouseleave events are used, when available
    (Firefox, IE). For delegation however, it should has to use mouseover/mouseout
    because it should still fire each time an element is entered or left so it can
    check if it matches the selector.
    
    thanks @luisateixeira for the research/fix

--- Source/Element/Element.Event.js
+++ Source/Element/Element.Event.js
@@ -146,16 +146,17 @@ Element.Events = {mousewheel: {
 	base: (Browser.firefox) ? 'DOMMouseScroll' : 'mousewheel'
 }};
 
+var check = function(event){
+	var related = event.relatedTarget;
+	if (related == null) return true;
+	if (!related) return false;
+	return (related != this && related.prefix != 'xul' && typeOf(this) != 'document' && !this.contains(related));
+};
+
 if ('onmouseenter' in document.documentElement){
 	Element.NativeEvents.mouseenter = Element.NativeEvents.mouseleave = 2;
+	Element.MouseenterCheck = check;
 } else {
-	var check = function(event){
-		var related = event.relatedTarget;
-		if (related == null) return true;
-		if (!related) return false;
-		return (related != this && related.prefix != 'xul' && typeOf(this) != 'document' && !this.contains(related));
-	};
-
 	Element.Events.mouseenter = {
 		base: 'mouseover',
 		condition: check
