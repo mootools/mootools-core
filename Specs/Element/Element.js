df48dcb8ee312a8aa56271d885b30bd85a2e9321
Arian Stolwijk <stolwijk.arian@gmail.com>
2013-11-21 00:25:19 +0100

    Add some tests with some common 'wrong' cases

--- Specs/Element/Element.js
+++ Specs/Element/Element.js
@@ -2497,3 +2497,29 @@ describe('new Element(expression)', function(){
 	});
 
 });
+
+describe('Element', function(){
+
+	describe('classList', function(){
+
+		it('should not fail for empty strings', function(){
+			var element = new Element('div');
+			element.addClass('');
+			expect(element.className).toEqual('');
+		});
+
+		it('should trim whitespaces', function(){
+			var element = new Element('div');
+			element.addClass(' bar ');
+			expect(element.className).toEqual('bar');
+		});
+
+		it('should add multiple classes', function(){
+			var element = new Element('div');
+			element.addClass(' bar foo');
+			expect(element.className).toEqual('bar foo');
+		});
+
+	});
+
+});
