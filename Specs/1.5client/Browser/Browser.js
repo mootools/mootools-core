293cb94d2f4493e3e45ca08629e075c3afc8de6b
Olmo Maldonado <olmo.maldonado@gmail.com>
2014-02-16 01:53:11 -0600

    Fixing Browser.ie for new IE11 UA string.

--- Specs/1.5client/Browser/Browser.js
+++ Specs/1.5client/Browser/Browser.js
@@ -59,15 +59,14 @@ describe('Browser.parse', function(){
 				version: 11
 			}
 		},
-		// Uncomment once this is fixed
-		// ie11v2: {
-		// 	desc: 'Internet Explorer 11 v2',
-		// 	string: 'Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv 11.0) like Gecko',
-		// 	expect: {
-		// 		name: 'ie',
-		// 		version: 11
-		// 	}
-		// },
+		ie11v2: {
+			desc: 'Internet Explorer 11 v2',
+			string: 'Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv 11.0) like Gecko',
+			expect: {
+				name: 'ie',
+				version: 11
+			}
+		},
 		ieCompat: {
 			desc: 'Internet Explorer 10 in IE7 compatibility',
 			string: 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; Trident/6.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E)',
