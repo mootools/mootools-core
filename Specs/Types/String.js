f7a10e279988f67458d5ee9bfd3978531783f42f
Olmo Maldonado <olmo.maldonado@gmail.com>
2014-02-23 12:35:41 -0600

    moved browser related specs to browser from string.

--- Specs/Types/String.js
+++ Specs/Types/String.js
@@ -108,21 +108,6 @@ describe("String Methods", function(){
 		expect('#000000'.hexToRgb()).toEqual('rgb(0,0,0)');
 	});
 
-	// String.stripScripts
-
-	it('should strip all script tags from a string', function(){
-		expect('<div><script type="text/javascript" src="file.js"></script></div>'.stripScripts()).toEqual('<div></div>');
-	});
-
-	it('should execute the stripped tags from the string', function(){
-		expect('<div><script type="text/javascript"> var stripScriptsSpec = 42; </script></div>'.stripScripts(true)).toEqual('<div></div>');
-		expect(window.stripScriptsSpec).toEqual(42);
-		expect('<div><script>\n// <!--\nvar stripScriptsSpec = 24;\n//-->\n</script></div>'.stripScripts(true)).toEqual('<div></div>');
-		expect(window.stripScriptsSpec).toEqual(24);
-		expect('<div><script>\n/*<![CDATA[*/\nvar stripScriptsSpec = 4242;\n/*]]>*/</script></div>'.stripScripts(true)).toEqual('<div></div>');
-		expect(window.stripScriptsSpec).toEqual(4242);
-	});
-
 	// String.substitute
 
 	it('should substitute values from objects', function(){
