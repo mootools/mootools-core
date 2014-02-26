9fe5f87b608770a7a59f3f7a904f25297e4535eb
Garrick Cheung <garrick@garrickcheung.com>
2014-02-26 02:27:04 -0800

    Setup travis + sauce + karma

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,39 @@
 "use strict";
 
-module.exports = function(grunt){
+module.exports = function(grunt) {
 
-	grunt.loadNpmTasks('grunt-mootools-packager');
-	grunt.loadNpmTasks('grunt-karma');
-	grunt.loadNpmTasks('grunt-contrib-clean');
+	// grunt.loadNpmTasks('grunt-mootools-packager');
+	// grunt.loadNpmTasks('grunt-karma');
+	// grunt.loadNpmTasks('grunt-contrib-clean');
+	// grunt.loadNpmTasks('grunt-contrib-connect');
+
+	require('load-grunt-tasks')(grunt);
 
 	grunt.initConfig({
+		'connect': {
+			testserver: {
+				options: {
+					// We use end2end task (which does not start the webserver)
+					// and start the webserver as a separate process (in travis_build.sh)
+					// to avoid https://github.com/joyent/libuv/issues/826
+					port: 8000,
+					hostname: '0.0.0.0',
+					middleware: function(connect, options) {
+						return [
+							function(req, resp, next) {
+								// cache get requests to speed up tests on travis
+								if (req.method === 'GET') {
+									resp.setHeader('Cache-control', 'public, max-age=3600');
+								}
+
+								next();
+							},
+							connect.static(options.base)
+						];
+					}
+				}
+			}
+		},
 
 		'packager': {
 
@@ -51,16 +78,124 @@ module.exports = function(grunt){
 		'karma': {
 
 			options: {
-				browsers: ['PhantomJS'],
 				frameworks: ['jasmine', 'sinon'],
 				files: ['http://rawgithub.com/bitovi/legacy-syn/master/dist/syn.js', 'mootools-*.js']
 			},
 
 			continuous: {
-				singleRun: true
+				singleRun: true,
+				sauceLabs: {
+					username: process.env.SAUCE_USERNAME,
+					accessKey: process.env.SAUCE_ACCESS_KEY,
+					testName: 'MooTools-Core'
+				},
+				customLaunchers: {
+					chrome_linux: {
+						base: 'SauceLabs',
+						browserName: 'chrome',
+						platform: 'linux'
+					},
+					firefox_linux: {
+						base: 'SauceLabs',
+						browserName: 'firefox',
+						platform: 'linux'
+					},
+					opera_win2000: {
+						base: 'SauceLabs',
+						browserName: 'opera',
+				        platform: 'Windows 2008',
+				        version: '12'
+					},
+					safari7: {
+						base: 'SauceLabs',
+						browserName: 'safari',
+						version: '7'
+					},
+					safari6: {
+						base: 'SauceLabs',
+						browserName: 'safari',
+						version: '6'
+					},
+					safari5_osx10_6: {
+						base: 'SauceLabs',
+						browserName: 'safari',
+						version: '5',
+						platform: 'OS X 10.6'
+					},
+					safari5_win7: {
+						base: 'SauceLabs',
+						browserName: 'safari',
+						version: '5',
+						platform: 'Windows 7'
+					},
+					ie11: {
+						base: 'SauceLabs',
+						browserName: 'internet explorer',
+						version: '11'
+					},
+					ie10: {
+						base: 'SauceLabs',
+						browserName: 'internet explorer',
+						version: '10'
+					},
+					ie9: {
+						base: 'SauceLabs',
+						browserName: 'internet explorer',
+						version: '9'
+					},
+					ie8: {
+						base: 'SauceLabs',
+						browserName: 'internet explorer',
+						version: '8'
+					},
+					ie7: {
+						base: 'SauceLabs',
+						browserName: 'internet explorer',
+						version: '7'
+					},
+					ie6: {
+						base: 'SauceLabs',
+						browserName: 'internet explorer',
+						version: '6'
+					},
+					iphone_7: {
+						base: 'SauceLabs',
+						browserName: 'iphone',
+						version: '7'
+					},
+					iphone_6_1: {
+						base: 'SauceLabs',
+						browserName: 'iphone',
+						version: '6.1'
+					},
+					iphone_6: {
+						base: 'SauceLabs',
+						browserName: 'iphone',
+						version: '6'
+					}
+				},
+				browsers: [
+					'chrome_linux', 
+					'firefox_linux', 
+					'opera_win2000',
+					// 'safari7',
+					// 'safari6',
+					// 'safari5_osx10_6',
+					// 'safari5_win7',
+					// 'ie11',
+					// 'ie10',
+					// 'ie9',
+					// 'ie8',
+					// 'ie7',
+					// 'ie6',
+					// 'iphone_7',
+					// 'iphone_6_1',
+					// 'iphone_6'
+				]
 			},
 
 			dev: {
+				browsers: ['PhantomJS'],
 				reporters: 'dots'
 			}
 
@@ -75,5 +210,6 @@ module.exports = function(grunt){
 	});
 
 	grunt.registerTask('default', ['clean', 'packager:all', 'packager:specs', 'karma:continuous']);
-	grunt.registerTask('nocompat', ['clean', 'packager:nocompat', 'packager:specs-nocompat', 'karma:continuous'])
+	grunt.registerTask('nocompat', ['clean', 'packager:nocompat', 'packager:specs-nocompat', 'karma:continuous']);
+	grunt.registerTask('default:travis', ['clean', 'packager:all', 'packager:specs', 'karma:continuous'])
 };
