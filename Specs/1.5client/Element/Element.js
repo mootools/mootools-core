e2134dd46dca12e168b92f48efc35bec6355e6ae
SergioCrisostomo <sergiosbox@gmail.com>
2014-06-02 16:01:23 +0100

    update file structure

--- Specs/1.5client/Element/Element.js
+++
@@ -1,9 +0,0 @@
-
-describe('creating new Elements', function(){
-
-	it('should create an element with type="email"', function(){
-		var el = new Element('input', {type: 'email'});
-		expect(el.get('type')).toMatch(/email|text/);
-	});
-
-});
